0e1269019b4d180aef6840ae51124d2b4ab2a148
kgowtham <gowtham.kumaresan@telliant.net>
2020-06-23 16:17:01 +0530

    basic-setup module

--- Sourcecode/UI/StriveUI/src/app/shared/services/crud-operation.service.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/services/crud-operation.service.ts
@@ -5,8 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class CrudOperationService {
   customerdetails = [];
+  basicsetupdetails = [];
   constructor() { }
   getCustomerDetails() {
     return this.customerdetails;
+  }
+  getBasicSetupDetails(){
+    return this.basicsetupdetails;
   }
 }
