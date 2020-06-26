44654ee2e2746047ec8f6d4291567811cc05af53
Mohamed Siad <mohamed.siad@telliant.net>
2020-06-26 19:20:07 +0530

    firebase front end ios

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/AppDelegate.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/AppDelegate.cs
@@ -1,7 +1,14 @@
-﻿using Foundation;
+﻿using System;
+using Firebase.CloudMessaging;
+using Foundation;
+using MvvmCross;
+using MvvmCross.Navigation;
 using MvvmCross.Platforms.Ios.Core;
+using Plugin.FirebasePushNotification;
+using Strive.Core.ViewModels.Customer;
 using StriveCustomer.iOS.MvvmCross;
 using UIKit;
+using UserNotifications;
 
 namespace StriveCustomer.iOS
 {
@@ -37,6 +44,90 @@ namespace StriveCustomer.iOS
             // Called when the user discards a scene session.
             // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
             // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
+
+        public override bool WillFinishLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+
+            Firebase.Core.App.Configure();
+
+            FirebasePushNotificationManager.Initialize(launchOptions,true);
+
+            CrossFirebasePushNotification.Current.RegisterForPushNotifications();
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = new UserDelegate();
+
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
+            };
+
+            return true;
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            FirebasePushNotificationManager.DidRegisterRemoteNotifications(deviceToken);
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            FirebasePushNotificationManager.RemoteNotificationRegistrationFailed(error);
+
+        }
+        // To receive notifications in foregroung on iOS 9 and below.
+        // To receive notifications in background in any iOS version
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            // If you are receiving a notification message while your app is in the background,
+            // this callback will not be fired 'till the user taps on the notification launching the application.
+
+            // If you disable method swizzling, you'll need to call this method. 
+            // This lets FCM track message delivery and analytics, which is performed
+            // automatically with method swizzling enabled.
+            FirebasePushNotificationManager.DidReceiveMessage(userInfo);
+            // Do your magic to handle the notification data
+            System.Console.WriteLine(userInfo);
+
+            Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<LoginViewModel>();
+
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Opened");
+                foreach (var data in p.Data)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                }
+
+
+            };
+
+        }
+
+
+
+        public class UserDelegate : UNUserNotificationCenterDelegate
+        {
+             
         }
     }
 }
