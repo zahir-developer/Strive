eef9f4d6b0a22f73b5a8b2670d6b2b550f165617
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-07-01 10:57:12 -0400

    handled 401 error through interceptor

--- Sourcecode/UI/StriveUI/src/app/shared/interceptor/token.interceptor.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/interceptor/token.interceptor.ts
@@ -1,39 +1,59 @@
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { tap} from 'rxjs/operators';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+    constructor(private router: Router, private route: ActivatedRoute, private loginService: LoginService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-        setHeaders: {
-            Authorization: `Bearer ${localStorage.getItem('authorizationToken')}`,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
-            // 'Accept': 'application/json'
-        }
-    });
-    // return next.handle(request);
-    return next.handle(request).pipe(tap(event => { }, err => {
-        if (err instanceof HttpErrorResponse && err.status === 401) {
-            // handle 401 errors
-            // we need to logout the session and navigate to Login page
-            this.router.navigate([`/login`], { relativeTo: this.route });
-        }
-        if (err instanceof HttpErrorResponse && err.status === 500) {
-            // handle 500 errors
-        }
-    }));
-}
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // const token = localStorage.getItem('authorizationToken');
+        request = request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${localStorage.getItem('authorizationToken')}`,
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+                // 'Accept': 'application/json'
+            }
+        });
+        // return next.handle(request);
+        return next.handle(request).pipe(tap(event => { }, err => {
+            console.log(err);
+            if (err.status === 401) {
+                console.log(err, 'token expired');
+                const params = {
+                    token: localStorage.getItem('authorizationToken'),
+                    refreshToken: localStorage.getItem('refreshToken'),
+                };
+                return this.loginService.refreshToken(params).subscribe((data: any) => {
+                    const token = JSON.parse(data);
+                    if (data.statusCode === 200) {
+                        localStorage.setItem('authorizationToken', token.Token);
+                        localStorage.setItem('refreshToken', token.RefreshToken);
+                        request = request.clone({
+                            setHeaders: {
+                                Authorization: `Bearer ${localStorage.getItem('authorizationToken')}`,
+                                'Access-Control-Allow-Origin': '*',
+                                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+                                // 'Accept': 'application/json'
+                            }
+                        });
+                        return next.handle(request);
+                    }
+                });
+
+            } else {
+                this.router.navigate([`/login`], { relativeTo: this.route });
+            }
+        }));
+    }
 }
