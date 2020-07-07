819f92b9c9dc4c491e04138c40c67b74c5e2797c
kgowtham <gowtham.kumaresan@telliant.net>
2020-07-07 20:25:46 +0530

    system setup design

--- Sourcecode/UI/StriveUI/src/app/modules/admin/admin.module.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/admin/admin.module.ts
@@ -13,33 +13,17 @@ import {DialogModule} from 'primeng/dialog';
 import { SchedulingComponent } from './scheduling/scheduling.component';
 import {SharedModule} from 'src/app/shared/shared.module'
 import { SetupComponent } from './system-setup/setup.component';
-import { LocationSetupComponent } from './location-setup/location-setup.component';
-import { LocationSetupListComponent } from './location-setup/location-setup-list/location-setup-list.component';
-import { LocationCreateEditComponent } from './location-setup/location-create-edit/location-create-edit.component';
-import { ServiceSetupComponent } from './service-setup/service-setup.component';
-import { ServiceSetupListComponent } from './service-setup/service-setup-list/service-setup-list.component';
-import { ServiceCreateEditComponent } from './service-setup/service-create-edit/service-create-edit.component';
-import { ProductSetupComponent } from './product-setup/product-setup.component';
-import { ProductSetupListComponent } from './product-setup/product-setup-list/product-setup-list.component';
-import { ProductCreateEditComponent } from './product-setup/product-create-edit/product-create-edit.component';
-import { VendorSetupComponent } from './vendor-setup/vendor-setup.component';
-import { VendorSetupListComponent } from './vendor-setup/vendor-setup-list/vendor-setup-list.component';
-import { VendorCreateEditComponent } from './vendor-setup/vendor-create-edit/vendor-create-edit.component';
 import { ConfirmationService } from 'primeng/api';
 import { ThemeComponent } from './theme/theme.component';
 import { ThemeService } from 'src/app/shared/common-service/theme.service';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
-import { OnlynumberDirective } from 'src/app/shared/Directive/only-number.directive';
 import { CashRegisterComponent } from './cash-register/cash-register.component';
 import { CloseoutRegisterComponent } from './closeout-register/closeout-register.component';
 
 @NgModule({
   declarations: [AdminComponent, EmployeesComponent, EmployeeListComponent, CreateEditComponent, SchedulingComponent,
-  SetupComponent,LocationSetupComponent,LocationSetupListComponent,LocationCreateEditComponent,ServiceSetupComponent,ServiceSetupListComponent,
-  ServiceCreateEditComponent,ProductSetupComponent,ProductSetupListComponent,ProductCreateEditComponent,VendorSetupComponent,
-  VendorSetupListComponent,VendorCreateEditComponent, ThemeComponent, HeaderComponent, CardComponent],
-  VendorSetupListComponent,VendorCreateEditComponent,OnlynumberDirective,CashRegisterComponent,CloseoutRegisterComponent],
+     ThemeComponent, HeaderComponent, CardComponent,CashRegisterComponent,CloseoutRegisterComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
--- Sourcecode/UI/StriveUI/src/app/modules/admin/admin.routing.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/admin/admin.routing.ts
@@ -3,14 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { SchedulingComponent } from './scheduling/scheduling.component';
-import { SetupComponent } from './system-setup/setup.component';
-import { LocationSetupComponent } from './location-setup/location-setup.component';
-import { ServiceSetupComponent } from './service-setup/service-setup.component';
-import { ProductSetupComponent } from './product-setup/product-setup.component';
-import { VendorSetupComponent } from './vendor-setup/vendor-setup.component';
 import { CashRegisterComponent } from './cash-register/cash-register.component';
 import { CloseoutRegisterComponent } from './closeout-register/closeout-register.component';
-import { ProductSetupListComponent } from './product-setup/product-setup-list/product-setup-list.component';
 import { ThemeComponent } from './theme/theme.component';
 
 
@@ -19,15 +13,10 @@ const adminRoutes: Routes = [
       path: '', component: AdminComponent, children: [{
           path: 'employees', component: EmployeesComponent},
         {path: 'scheduling', component: SchedulingComponent},
-        {path: 'setup', component: SetupComponent},
-        {path: 'location', component: LocationSetupComponent},
         {path: 'theme', component: ThemeComponent},
-        {path: 'basic', component: BasicSetupComponent},
-        {path: 'service', component: ServiceSetupComponent},
-        {path: 'product', component: ProductSetupComponent},
-        {path: 'vendor', component: VendorSetupComponent},
         {path: 'cashregister', component: CashRegisterComponent},
-        {path: 'closeoutregister', component: CloseoutRegisterComponent}     
+        {path: 'closeoutregister', component: CloseoutRegisterComponent},
+        {path: 'setup', loadChildren: () => import('./system-setup/setup.module').then(mod => mod.SetupModule)},     
       ]
   }
 ];
