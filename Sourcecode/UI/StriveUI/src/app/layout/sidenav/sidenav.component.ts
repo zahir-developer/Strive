839b266d55b3ee761f6d9020bf547128467d0e1c
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-07-08 11:31:22 -0400

    sidenav and header navigation bar integration

--- Sourcecode/UI/StriveUI/src/app/layout/sidenav/sidenav.component.ts
+++ Sourcecode/UI/StriveUI/src/app/layout/sidenav/sidenav.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { UserDataService } from 'src/app/shared/util/user-data.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private user: UserDataService) { }
 
   ngOnInit(): void {
   }
+  openNav() {
+    document.getElementById('mySidebar').style.width = '200px';
+    document.getElementById('main').style.marginLeft = '200px';
+  }
 
+  closeNav() {
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
 }
