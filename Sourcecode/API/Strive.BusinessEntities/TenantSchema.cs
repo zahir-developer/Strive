a605ec478f26375ff33a7850c16ea5f9a546cf7b
Naveen Dhasarathan <naveenkumar.dhasarathan@telliant.net>
2020-07-10 01:47:32 +0530

    added Employee Login save

--- Sourcecode/API/Strive.BusinessEntities/TenantSchema.cs
+++ Sourcecode/API/Strive.BusinessEntities/TenantSchema.cs
@@ -11,6 +11,7 @@ namespace Strive.BusinessEntities
         public string Schemaname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public Guid TenantGuid { get; set; }
         public DateTime Firstlogin { get; set; }
         public DateTime Lastlogin { get; set; }
     }
