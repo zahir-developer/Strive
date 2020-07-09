76062722f498a92385ef13d46aeb845647c91b27
Priyanka Ramu <priyanka.ramu@telliant.net>
2020-07-09 15:45:22 +0530

    Vendor Changes

--- Sourcecode/API/Strive.BusinessEntities/CashRegister/CashRegister.cs
+++ Sourcecode/API/Strive.BusinessEntities/CashRegister/CashRegister.cs
@@ -12,14 +12,14 @@ namespace Strive.BusinessEntities.CashRegister
         public int? LocationId { get; set; }
         public int? DrawerId { get; set; }
         public int? UserId { get; set; }
-
+        [Column(TypeName = "datetime")]
+        public DateTime? EnteredDateTime { get; set; }
         public int? CashRegCoinId { get; set; }
         public int? CashRegRollId { get; set; }
         public int? CashRegBillId { get; set; }
         public int? CashRegOtherId { get; set; }
 
-        [Column(TypeName = "datetime")]
-        public DateTime? EnteredDateTime { get; set; }
+     
     }
 }
 
