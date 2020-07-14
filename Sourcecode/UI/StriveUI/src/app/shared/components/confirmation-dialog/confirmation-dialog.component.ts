f77310f5e1d9b23864aa6261491091d926872ff6
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-07-14 10:09:24 -0400

    created confirmationService Dialog and integrated in location setup

---
+++ Sourcecode/UI/StriveUI/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css']
+})
+export class ConfirmationDialogComponent implements OnInit {
+  @Input() title: string;
+  @Input() message: string;
+  @Input() btnOkText: string;
+  @Input() btnNoText: string;
+  @Input() btnCancelText: string;
+  @Input() cancelBtn = false;
+
+  constructor(private activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+  public accept() {
+    this.activeModal.close(true);
+  }
+
+  public dismiss() {
+    this.activeModal.dismiss();
+  }
+  public cancel() {
+     this.activeModal.dismiss(false);
+  }
+
+
+}
+
