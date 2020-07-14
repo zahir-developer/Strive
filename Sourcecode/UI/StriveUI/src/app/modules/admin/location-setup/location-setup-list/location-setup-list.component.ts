c3028b7c1e28824d42737d2a34e17ff8e7542123
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-07-14 00:49:53 -0400

    button align done

--- Sourcecode/UI/StriveUI/src/app/modules/admin/location-setup/location-setup-list/location-setup-list.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/admin/location-setup/location-setup-list/location-setup-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { ConfirmationService } from 'primeng/api';
 import { LocationService } from 'src/app/shared/services/data-service/location.service';
 import { ToastrService } from 'ngx-toastr';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-location-setup-list',
   templateUrl: './location-setup-list.component.html',
-  styleUrls: ['./location-setup-list.component.css']
+  styleUrls: ['./location-setup-list.component.css'],
+  providers: [ConfirmationService]
 })
 export class LocationSetupListComponent implements OnInit {
   locationSetupDetails = [];
@@ -15,72 +16,86 @@ export class LocationSetupListComponent implements OnInit {
   showDialog = false;
   selectedData: any;
   headerData: string;
+  searchByName = '';
+  searchById = '';
   isEdit: boolean;
   isTableEmpty: boolean;
-  constructor(private locationService: LocationService, private toastr: ToastrService, private fb: FormBuilder, private confirmationService: ConfirmationService) { }
+  constructor(private locationService: LocationService, private toastr: ToastrService, private fb: FormBuilder,
+              private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
     this.locationSetupForm = this.fb.group({
-      workHour: ['', Validators.required,Validators.maxLength(2)]
+      workHour: ['', Validators.required, Validators.maxLength(2)]
     });
-    
+
     this.getAllLocationSetupDetails();
-    
+
   }
   getAllLocationSetupDetails() {
-    //this.locationSetupDetails=this.crudService.getlocationSetupDetails();
+    // this.locationSetupDetails=this.crudService.getlocationSetupDetails();
     this.locationService.getLocation().subscribe(data => {
       if (data.status === 'Success') {
         const location = JSON.parse(data.resultData);
         this.locationSetupDetails = location.Location;
         console.log(this.locationSetupDetails);
-        if(this.locationSetupDetails.length === 0){
+        if (this.locationSetupDetails.length === 0) {
           this.isTableEmpty = true;
-        }else{
+        } else {
           this.isTableEmpty = false;
         }
       }
     });
   }
-edit(data) {
-this.selectedData = data;
-this.showDialog = true;
-}
-delete(data) {
-  //const index = this.locationSetupDetails.map(x => x.id).indexOf(data.id);
+  edit(data) {
+    this.selectedData = data;
+    this.showDialog = true;
+  }
+  delete(data) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Yes',
+      rejectLabel: 'No',
+      accept: () => {
+        this.locationService.deleteLocation(data.LocationId).subscribe(res => {
+          if (res.status === 'Success') {
+            this.toastr.success('Record Deleted Successfully!!', 'Success!');
+          }
+        });
+      },
+ reject: () => {
+      }
+    });
+    // const index = this.locationSetupDetails.map(x => x.id).indexOf(data.id);
     // this.confirmationService.confirm({
     //   header: 'Delete',
     //   message: 'Do you want to continue?',
     //   acceptLabel: 'Yes',
     //   rejectLabel: 'Cancel',
     //   accept: () => {
-        this.locationService.deleteLocation(data.LocationId).subscribe(res => {
-            if(res.status === "Success"){
-              this.toastr.success('Record Deleted Successfully!!', 'Success!');
-            }
-        })
     //   },
     //   reject: () => {
     //   }
-    // });    
-}
-closePopupEmit(event) {
-  if(event.status === 'saved') {
-    this.getAllLocationSetupDetails();
+    // });
   }
-  this.showDialog = event.isOpenPopup;
-}
-add( data, locationDetails?) {
-  if (data === 'add') {
-    this.headerData = 'Add New Location';
-    this.selectedData = locationDetails;
-    this.isEdit = false;
-    this.showDialog = true;
-  } else {
-    this.headerData = 'Edit Location';
-    this.selectedData = locationDetails;
-    this.isEdit = true;
-    this.showDialog = true;
+  closePopupEmit(event) {
+    if (event.status === 'saved') {
+      this.getAllLocationSetupDetails();
+    }
+    this.showDialog = event.isOpenPopup;
   }
-}
+  add(data, locationDetails?) {
+    if (data === 'add') {
+      this.headerData = 'Add New Location';
+      this.selectedData = locationDetails;
+      this.isEdit = false;
+      this.showDialog = true;
+    } else {
+      this.headerData = 'Edit Location';
+      this.selectedData = locationDetails;
+      this.isEdit = true;
+      this.showDialog = true;
+    }
+  }
 }
