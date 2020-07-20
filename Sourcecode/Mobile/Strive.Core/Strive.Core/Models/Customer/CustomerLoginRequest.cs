9d81cf471b7a1cf0dda3a57f92a9eec442db0627
Ganesh <ganesh.kubendrababu@telliant.net>
2020-07-20 14:30:19 +0530

    Integration of loginAPI and some other extra changes..

---
+++ Sourcecode/Mobile/Strive.Core/Strive.Core/Models/Customer/CustomerLoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strive.Core.Models.Customer
+{
+   public class CustomerLoginRequest
+    {
+        public CustomerLoginRequest(string email, string passwordHash)
+            {
+                this.email = email;
+                this.passwordHash = passwordHash;
+            }
+        public string email { get; set; }
+
+        public string passwordHash { get; set; }
+    }
+}
--- Sourcecode/Mobile/Strive.Core/Strive.Core/Services/Interfaces/IAdminService.cs
+++ Sourcecode/Mobile/Strive.Core/Strive.Core/Services/Interfaces/IAdminService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Strive.Core.Models;
+using Strive.Core.Models.Customer;
 using Strive.Core.Models.TimInventory;
 
 namespace Strive.Core.Services.Interfaces
@@ -10,5 +11,7 @@ namespace Strive.Core.Services.Interfaces
         Task<object> Login(string username, string password);
 
         Task<EmployeeResultData> EmployeeLogin(EmployeeLoginRequest request);
+
+        Task<EmployeeResultData> CustomerLogin(CustomerLoginRequest loginRequest);
     }
 }
