a789dae6281a4172e711dc61ae1893338bd1a4ab
Naveen Dhasarathan <naveenkumar.dhasarathan@telliant.net>
2020-07-27 20:09:37 +0530

    Added Messenger related Tables

---
+++ Sourcecode/Database/StriveTenantNew/StriveTenantNew/StriveCarSalon/Tables/tblChatMessageRecipient.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [StriveCarSalon].[tblChatMessageRecipient] (
+    [ChatRecipientId]  BIGINT IDENTITY (1, 1) NOT NULL,
+    [ChatMessageId]    BIGINT NULL,
+    [RecipientGroupId] INT    NULL,
+    [IsRead]           BIT    NULL,
+    [SenderId]         INT    NULL,
+    CONSTRAINT [PK_tblChatMessageRecipient] PRIMARY KEY CLUSTERED ([ChatRecipientId] ASC),
+    CONSTRAINT [FK_tblChatMessageRecipient_tblChatMessage] FOREIGN KEY ([ChatMessageId]) REFERENCES [StriveCarSalon].[tblChatMessage] ([ChatMessageId]),
+    CONSTRAINT [FK_tblChatMessageRecipient_tblChatUserGroup] FOREIGN KEY ([RecipientGroupId]) REFERENCES [StriveCarSalon].[tblChatUserGroup] ([ChatGroupUserId]),
+    CONSTRAINT [FK_tblChatMessageRecipient_tblEmployee] FOREIGN KEY ([SenderId]) REFERENCES [StriveCarSalon].[tblEmployee] ([EmployeeId])
+);
+
---
+++ Sourcecode/Database/StriveTenantNew/StriveTenantNew/StriveCarSalon/Tables/tblChatUserGroup.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [StriveCarSalon].[tblChatUserGroup] (
+    [ChatGroupUserId] INT IDENTITY (1, 1) NOT NULL,
+    [UserId]          INT NULL,
+    [GroupId]         INT NULL,
+    [IsActive]        BIT NULL,
+    [IsDeleted]       BIT NULL,
+    [CreatedBy]       INT NULL,
+    [CreatedDate]     INT NULL,
+    CONSTRAINT [PK_tblChatUserGroup] PRIMARY KEY CLUSTERED ([ChatGroupUserId] ASC),
+    CONSTRAINT [FK_tblChatUserGroup_tblChatGroup] FOREIGN KEY ([GroupId]) REFERENCES [StriveCarSalon].[tblChatGroup] ([ChatGroupId]),
+    CONSTRAINT [FK_tblChatUserGroup_tblEmployee] FOREIGN KEY ([UserId]) REFERENCES [StriveCarSalon].[tblEmployee] ([EmployeeId])
+);
+
