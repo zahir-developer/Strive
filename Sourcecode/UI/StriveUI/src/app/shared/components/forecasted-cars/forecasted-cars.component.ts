bd35c96f360ef4d17f7b81823b20f59cfb1e2776
kgowtham <gowtham.kumaresan@telliant.net>
2020-08-04 13:39:34 +0530

    shared components Client

---
+++ Sourcecode/UI/StriveUI/src/app/shared/components/forecasted-cars/forecasted-cars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forecasted-cars',
+  templateUrl: './forecasted-cars.component.html',
+  styleUrls: ['./forecasted-cars.component.css']
+})
+export class ForecastedCarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Sourcecode/UI/StriveUI/src/app/shared/components/no-of-washes/no-of-washes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-of-washes',
+  templateUrl: './no-of-washes.component.html',
+  styleUrls: ['./no-of-washes.component.css']
+})
+export class NoOfWashesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
