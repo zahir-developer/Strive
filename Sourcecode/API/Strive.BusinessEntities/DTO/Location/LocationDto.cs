a2c82f3ad0bd9b1bb86529cd2286be6fe4cb65d8
vbalasubramanian <vineeth.balasubramanian@telliant.net>
2020-08-06 21:20:50 +0530

    Code modified in new approaches for collision and location

--- Sourcecode/API/Strive.BusinessEntities/DTO/Location/LocationDto.cs
+++ Sourcecode/API/Strive.BusinessEntities/DTO/Location/LocationDto.cs
@@ -1,11 +1,12 @@
 ﻿using Strive.BusinessEntities.Model;
+using System.Collections.Generic;
 
 namespace Strive.BusinessEntities.DTO
 {
     public class LocationDto
     {
         public Model.Location Location { get; set; }
-        public LocationAddress LocationAddress { get; set; }
+        public List<LocationAddress> LocationAddress { get; set; }
         public Drawer Drawer { get; set; }
     }
 }
