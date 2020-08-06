992b4d1159e89ad8a4b810457229615e990887e3
Naveen Kumar Dhasarathan <naveenkumar.dhasarathan@telliant.net>
2020-08-07 00:20:10 +0530

    Added TotalAmount field

--- Sourcecode/API/Strive.BusinessEntities/Model/EmployeeLiability.cs
+++ Sourcecode/API/Strive.BusinessEntities/Model/EmployeeLiability.cs
@@ -3,45 +3,48 @@ using System;
 
 namespace Strive.BusinessEntities.Model
 {
-[OverrideName("tblEmployeeLiability")]
-public class EmployeeLiability
-{
+    [OverrideName("tblEmployeeLiability")]
+    public class EmployeeLiability
+    {
 
-	[Column, PrimaryKey, IgnoreOnInsert, IgnoreOnUpdate]
-	public int LiabilityId { get; set; }
+        [Column, PrimaryKey, IgnoreOnInsert, IgnoreOnUpdate]
+        public int LiabilityId { get; set; }
 
-	[Column, PrimaryKey]
-	public int? EmployeeId { get; set; }
+        [Column, PrimaryKey]
+        public int? EmployeeId { get; set; }
 
-	[Column]
-	public int LiabilityType { get; set; }
+        [Column]
+        public int LiabilityType { get; set; }
 
-	[Column]
-	public string LiabilityDescription { get; set; }
+        [Column]
+        public string LiabilityDescription { get; set; }
 
-	[Column]
-	public int? ProductId { get; set; }
+        [Column]
+        public int? ProductId { get; set; }
 
-	[Column]
-	public int? Status { get; set; }
+        [Column]
+        public decimal? TotalAmount { get; set; }
 
-	[Column]
-	public bool? IsActive { get; set; }
+        [Column]
+        public int? Status { get; set; }
 
-	[Column]
-	public bool? IsDeleted { get; set; }
+        [Column]
+        public bool? IsActive { get; set; }
 
-	[Column]
-	public int? CreatedBy { get; set; }
+        [Column]
+        public bool? IsDeleted { get; set; }
 
-	[Column]
-	public DateTimeOffset? CreatedDate { get; set; }
+        [Column]
+        public int? CreatedBy { get; set; }
 
-	[Column]
-	public int? UpdatedBy { get; set; }
+        [Column]
+        public DateTimeOffset? CreatedDate { get; set; }
 
-	[Column]
-	public DateTimeOffset? UpdatedDate { get; set; }
+        [Column]
+        public int? UpdatedBy { get; set; }
 
-}
+        [Column]
+        public DateTimeOffset? UpdatedDate { get; set; }
+
+    }
 }
