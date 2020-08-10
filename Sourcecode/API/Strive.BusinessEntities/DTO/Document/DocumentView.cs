83d481b3cf7f5473452a29cd7cbb9efdb615b50c
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2020-08-11 03:14:25 +0530

    API - GetDocument / GetEmployee Issue Fixed. *API New Approach changes made.

--- Sourcecode/API/Strive.BusinessEntities/DTO/Document/DocumentView.cs
+++ Sourcecode/API/Strive.BusinessEntities/DTO/Document/DocumentView.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Strive.BusinessEntities.Document
 {
-    public class DocumentView
+    public class DocumentViewModel
     {
-        public long DocumentId { get; set; }
+        public long EmployeeDocumentId { get; set; }
         public long EmployeeId { get; set; }
         public string FileName { get; set; }
-        public string FilePath { get; set; }
+        public string FileType { get; set; }
         public string Password { get; set; }
+        public bool IsPasswordProtected { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
         public bool IsActive { get; set; }
         public string Base64Url { get; set; }
-
     }
 }
