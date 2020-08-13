50ffac659b5e59e67db09e1317c3ef8f018b5541
kgowtham <gowtham.kumaresan@telliant.net>
2020-08-13 14:03:16 +0530

    dashboard url

--- Sourcecode/UI/StriveUI/src/app/shared/services/data-service/wash.service.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/services/data-service/wash.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpUtilsService } from '../../util/http-utils.service';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { UrlConfig } from '../url.config';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WashService {
+export class WashService {  
+public data: BehaviorSubject<string> = new BehaviorSubject('');
+  dashboardData: any;
 
   constructor(private http: HttpUtilsService) { }
   getAllWashes(): Observable<any> {
@@ -23,5 +25,12 @@ export class WashService {
   }
   deleteWash(id : number){
     return this.http.get(`${UrlConfig.totalUrl.deleteWash}` + id);
+  }
+  getDashboard() {
+    const locationId = 1;
+    this.http.get(`${UrlConfig.totalUrl.getDashBoardCount}` + locationId).subscribe((data: any) => {
+      this.dashboardData = data.currentWeather;
+      this.data.next(this.dashboardData);
+    });
   }
 }
