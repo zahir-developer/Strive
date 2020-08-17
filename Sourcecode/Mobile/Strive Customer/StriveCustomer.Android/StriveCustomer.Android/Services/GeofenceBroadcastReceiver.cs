406b12caac49f0842b4bf774e0339de1e850431d
Ganesh <ganesh.kubendrababu@telliant.net>
2020-08-17 19:02:09 +0530

    Support notification for Android API less than 26...

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Services/GeofenceBroadcastReceiver.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Services/GeofenceBroadcastReceiver.cs
@@ -16,6 +16,8 @@ using Uri = Android.Net.Uri;
 using Android.Support.V4.App;
 using Android.Graphics;
 using StriveCustomer.Android.Fragments;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+using StriveCustomer.Android.Views;
 
 namespace StriveCustomer.Android.Services
 {
@@ -56,7 +58,6 @@ namespace StriveCustomer.Android.Services
                     break;
             }
             setUpNotification(context);
-            buildSendNotification(mapIntent,context);
         }
 
          void setUpNotification(Context context)
@@ -65,14 +66,32 @@ namespace StriveCustomer.Android.Services
             mapIntent = new Intent(Intent.ActionView, googleMapUri);
             mapIntent.SetPackage("com.google.android.apps.maps");
             mapIntent.SetFlags(ActivityFlags.NewTask);
-            
-            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationImportance.High);
-            notificationChannel.EnableLights(true);
-            notificationChannel.EnableVibration(true);
-            notificationChannel.Description = "channel to trigger geofence notification";
-            notificationChannel.LockscreenVisibility = NotificationVisibility.Public;
-            notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
-            notificationManager.CreateNotificationChannel(notificationChannel);
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                var stackBuilder = TaskStackBuilder.Create(context);
+                stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(DashboardView)));
+                stackBuilder.AddNextIntent(mapIntent);
+                PendingIntent pendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+                var builder = new NotificationCompat.Builder(context);
+                builder.SetSmallIcon(Resource.Drawable.world_location)
+                                              .SetPriority(NotificationCompat.PriorityHigh)
+                                              .SetContentTitle("Car wash near by")
+                                              .SetAutoCancel(true)
+                                              .SetContentIntent(pendingIntent);
+                notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
+                notificationManager.Notify(0, builder.Build());
+            }
+            else
+            {
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationImportance.High);
+                notificationChannel.EnableLights(true);
+                notificationChannel.EnableVibration(true);
+                notificationChannel.Description = "channel to trigger geofence notification";
+                notificationChannel.LockscreenVisibility = NotificationVisibility.Public;
+                notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
+                notificationManager.CreateNotificationChannel(notificationChannel);
+                buildSendNotification(mapIntent, context);
+            }
         }      
         void buildSendNotification(Intent mapIntent,Context context) 
         {
