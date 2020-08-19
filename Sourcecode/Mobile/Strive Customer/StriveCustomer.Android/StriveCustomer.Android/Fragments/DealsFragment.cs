c6303bc42efee1139bc307fa919685498f209306
Ganesh <ganesh.kubendrababu@telliant.net>
2020-08-19 14:24:29 +0530

    dealsUI with common fixes..

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Fragments/DealsFragment.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Fragments/DealsFragment.cs
@@ -7,17 +7,21 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.Widget;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using Strive.Core.ViewModels.Customer;
+using StriveCustomer.Android.Adapter;
 
 namespace StriveCustomer.Android.Fragments
 {
     public class DealsFragment : MvxFragment<DealsViewModel>
     {
+        public List<string> listData;
+        Context context;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -31,8 +35,18 @@ namespace StriveCustomer.Android.Fragments
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
             var ignore = base.OnCreateView(inflater, container, savedInstanceState);
             var rootview = this.BindingInflate(Resource.Layout.DealsScreenFragment, null);
+            listData = new List<string>();
+            for(int i = 1;i<=10;i++)
+            {
+                listData.Add("Clickme"+i);
+            }
+            var dealsRecyclerView = rootview.FindViewById<RecyclerView>(Resource.Id.dealsList);
+            dealsRecyclerView.HasFixedSize = true;
+            var layoutManager = new LinearLayoutManager(context);
+            dealsRecyclerView.SetLayoutManager(layoutManager);
+            DealsAdapter dealsAdapter = new DealsAdapter(listData,context);
+            dealsRecyclerView.SetAdapter(dealsAdapter);
             return rootview;
-
         }
     }
 }
