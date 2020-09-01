7f12552b48d8559afb866bd1b4a306c1c8fa4b7b
Zahir Mohamed <zahir.mohamed@telliant.net>
2020-09-01 10:22:39 +0000

    Revert "Merge branch 'Strive_API_Dev' of https://gitlab.com/telliant-mammoth/strive into Strive_API_Dev"
    This reverts commit ef20d4f82927c7d53ee036e91fc7792183f6893b

--- Sourcecode/UI/StriveUI/src/app/modules/admin/system-setup/membership/membership-create-edit/membership-create-edit.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/admin/system-setup/membership/membership-create-edit/membership-create-edit.component.ts
@@ -89,8 +89,7 @@ export class MembershipCreateEditComponent implements OnInit {
     let service = [];
     this.membershipForm.patchValue({
       membershipName: this.selectedData.Membership.MembershipName,
-      notes: this.selectedData?.Membership?.Notes,
-      price: this.selectedData?.membership?.Price,
+      notes: this.selectedData.Membership.Notes,
       status: this.selectedData.Membership.IsActive === true ? 0 : 1
     });
     if (this.selectedData.MembershipService.filter(i => Number(i.ServiceTypeId) === 15)[0] !== undefined) {
@@ -126,8 +125,6 @@ export class MembershipCreateEditComponent implements OnInit {
       isActive: true,
       isDeleted: false,
       createdBy: 1,
-      price: this.membershipForm.value.price,
-      notes: this.membershipForm.value.notes,
       createdDate: new Date(),
       updatedBy: 1,
       updatedDate: new Date()
