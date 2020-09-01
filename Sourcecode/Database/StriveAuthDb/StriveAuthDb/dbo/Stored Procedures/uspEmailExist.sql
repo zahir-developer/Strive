f44d5ed7210f926956dd376271732a33d8170c79
Naveen Kumar Dhasarathan <naveenkumar.dhasarathan@telliant.net>
2020-09-01 19:08:59 +0530

    Auth DB Sync.

---
+++ Sourcecode/Database/StriveAuthDb/StriveAuthDb/dbo/Stored Procedures/uspEmailExist.sql
@@ -0,0 +1,23 @@
+﻿
+
+
+CREATE PROCEDURE [dbo].[uspEmailExist]
+(@Email varchar(50))
+
+AS 
+BEGIN
+
+DECLARE @Count int;
+SELECT @Count = (SELECT COUNT(1)
+FROM tblAuthMaster
+WHERE EmailId=@Email)
+
+IF @Count >0
+BEGIN
+SELECT EmailExist = 1;
+END
+else
+BEGIN
+SELECT EmailExist = 0;
+END
+END
