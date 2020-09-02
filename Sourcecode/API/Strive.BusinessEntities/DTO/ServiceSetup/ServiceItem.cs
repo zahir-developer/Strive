ee3a198871f714463826f19310cb2a79cc6d92e2
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2020-09-02 15:26:26 +0530

    Added ingnored item serviceItem.cs

---
+++ Sourcecode/API/Strive.BusinessEntities/DTO/ServiceSetup/ServiceItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.DTO.ServiceSetup
+{
+    public class ServiceItem
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public int ServiceType { get; set; }
+        public string ServiceTypeName { get; set; }
+        public string Upcharges { get; set; }
+
+    }
+}
