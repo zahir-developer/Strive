88aa066eafacd5bdca1caf55f6de5b78cb30129c
vbalasubramanian <vineeth.balasubramanian@telliant.net>
2020-09-02 17:29:03 +0530

    Details changes added

---
+++ Sourcecode/API/Strive.BusinessLogic/Details/IDetailsBpl.cs
@@ -0,0 +1,17 @@
+﻿using Strive.BusinessEntities.DTO;
+using Strive.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessLogic.Details
+{
+    public interface IDetailsBpl
+    {
+        Result GetDetailsById(int id);
+        Result AddDetails(DetailsDto details);
+        Result UpdateDetails(DetailsDto details);
+    }
+}
