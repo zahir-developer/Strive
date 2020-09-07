fa3fc5569f50fc116e83f5ac80242a28bfa0ea40
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-09-07 07:52:19 -0400

    design cleanup in sales

--- Sourcecode/UI/StriveUI/src/app/modules/sales/sales.module.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/sales/sales.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SalesRoutingModule} from './sales.routing';
 import {SalesComponent} from './sales/sales.component';
+import { SearchItemComponent } from './sales/search-item/search-item.component';
 
 
 
 
 @NgModule({
-  declarations: [SalesComponent],
+  declarations: [SalesComponent, SearchItemComponent],
   imports: [
     CommonModule,
     SalesRoutingModule
