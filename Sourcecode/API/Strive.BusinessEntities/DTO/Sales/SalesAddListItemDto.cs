b07e0c54b25cda8caa38170a8e3752f6144321ce
sprabhakaran <sprabhakaran@TELLRENTMM-15>
2020-09-08 17:11:15 +0530

    Api for AddApayment sales and changes in getticketno

---
+++ Sourcecode/API/Strive.BusinessEntities/DTO/Sales/SalesAddListItemDto.cs
@@ -0,0 +1,15 @@
+﻿using Strive.BusinessEntities.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.DTO.Sales
+{
+    public class SalesAddListItemDto
+    {
+        public Job Job { get; set; }
+        public JobItem JobItem { get; set; }
+    }
+}
