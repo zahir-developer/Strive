73f88dbdead743e8b006aecb83cb72021d900853
sprabhakaran <sprabhakaran@TELLRENTMM-15>
2020-09-09 23:04:06 +0530

    Api Changes and Models Class Changes

---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/ScheduleItemSummaryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class ScheduleItemSummaryViewModel
+    {
+        public decimal? Tax { get; set; }
+        public decimal? Cashback { get; set; }
+        public decimal? Price { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? GiftCard { get; set; }
+        public decimal? Total { get; set; }
+    }
+}
---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/ScheduleItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class ScheduleItemViewModel
+    {
+        public int JobId { get; set; }
+        public string TicketNumber { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceType { get; set; }
+
+    }
+}
