4d41aba969423d9f187c74f04dd02264e1e56874
vbalasubramanian <vineeth.balasubramanian@telliant.net>
2020-09-09 16:02:41 +0530

    Bayschedule model class added

---
+++ Sourcecode/API/Strive.BusinessEntities/Model/BaySchedule.cs
@@ -0,0 +1,47 @@
+﻿using Cocoon.ORM;
+using System;
+
+namespace Strive.BusinessEntities.Model
+{
+    [OverrideName("tblBaySchedule")]
+    public class BaySchedule
+    {
+
+        [Column, PrimaryKey, IgnoreOnInsert, IgnoreOnUpdate]
+        public int BayScheduleID { get; set; }
+
+        [Column]
+        public int BayId { get; set; }
+
+        [Column]
+        public int? JobId { get; set; }
+
+        [Column]
+        public DateTime? ScheduleDate { get; set; }
+
+        [Column]
+        public TimeSpan? ScheduleInTime { get; set; }
+
+        [Column]
+        public TimeSpan? ScheduleOutTime { get; set; }
+
+        [Column]
+        public bool? IsActive { get; set; }
+
+        [Column]
+        public bool? IsDeleted { get; set; }
+
+        [Column]
+        public int? CreatedBy { get; set; }
+
+        [Column]
+        public DateTimeOffset? CreatedDate { get; set; }
+
+        [Column]
+        public int? UpdatedBy { get; set; }
+
+        [Column]
+        public DateTimeOffset? UpdatedDate { get; set; }
+
+    }
+}
