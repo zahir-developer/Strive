a993d79d29067842b9105e151be4e54e5b5ae8d1
sprabhakaran <sprabhakaran@TELLRENTMM-15>
2020-09-10 15:13:22 +0530

    Api changes

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/ScheduleItemSummaryViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/ScheduleItemSummaryViewModel.cs
@@ -12,7 +12,6 @@ namespace Strive.BusinessEntities.ViewModel
         public decimal? Cashback { get; set; }
         public decimal? Price { get; set; }
         public int? Quantity { get; set; }
-        public decimal? GiftCard { get; set; }
         public decimal? Total { get; set; }
     }
 }
