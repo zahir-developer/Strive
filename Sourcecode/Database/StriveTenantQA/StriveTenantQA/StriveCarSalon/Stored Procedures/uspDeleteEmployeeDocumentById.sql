cb32bd22c1f65c431dd1d1bd7b250c851dc28b78
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2020-09-15 01:19:04 +0530

    Strive Tenant QA database script.

---
+++ Sourcecode/Database/StriveTenantQA/StriveTenantQA/StriveCarSalon/Stored Procedures/uspDeleteEmployeeDocumentById.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE PROCEDURE [StriveCarSalon].[uspDeleteEmployeeDocumentById] 
+@DocumentId int
+AS
+begin
+UPDATE [StriveCarSalon].[tblEmployeeDocument] SET IsDeleted = 1
+WHERE EmployeeDocumentId=@DocumentId
+end
