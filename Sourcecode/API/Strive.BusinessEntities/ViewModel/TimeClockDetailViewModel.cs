4de368638ec2868ad2d7355295bb3bbe37e93717
vbalasubramanian <vineeth.balasubramanian@telliant.net>
2020-09-23 17:31:10 +0530

    TimeClockEmployeeDetailS API change for duplicate employee adding for same date in Timeclock

---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/TimeClockDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class TimeClockDetailViewModel
+    {
+        public List<TimeClockEmployeeDetailViewModel> TimeClockEmployeeDetailViewModel { get; set; }
+        public List<NotClockedInEmployeeViewModel> EmployeeViewModel { get; set; }
+    }
+}
