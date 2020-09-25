cf226f44834bec9a9ef86b77e818eaa58d24424f
Ananth Subburaj <ananth.subburaj@telliant.net>
2020-09-25 16:13:04 +0530

    checkout ui added

---
+++ Sourcecode/UI/StriveUI/src/app/modules/checkout/checkout-grid/checkout-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-grid',
+  templateUrl: './checkout-grid.component.html',
+  styleUrls: ['./checkout-grid.component.css']
+})
+export class CheckoutGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
