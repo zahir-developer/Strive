c3174d0fd264334aa1d3b2afcca71045e097b86e
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2020-10-02 00:10:01 +0530

    Strive Tenant database changes - Sprint 3

--- Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetTimeClockEmployeeDetails.sql
+++ Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetTimeClockEmployeeDetails.sql
@@ -1,8 +1,15 @@
-﻿-- =============================================
+﻿
+-- =============================================
 -- Author:		Zahir Hussain
 -- Create date: 09-15-2020
 -- Description:	Retrieves all list of employees clocked based on location, EXEC [StriveCarSalon].uspGetTimeClockEmployeeDetails 0, '2020-08-15', '2020-09-09'
 -- =============================================
+
+-----------------------------------------------------------------------------------------
+-- Rev | Date Modified  | Developer	| Change Summary
+-----------------------------------------------------------------------------------------
+--  1  |  2020-Sep-30   | Vineeth	| Added IsActive and IsDelete condition for Employee
+-----------------------------------------------------------------------------------------
 CREATE PROCEDURE [StriveCarSalon].[uspGetTimeClockEmployeeDetails] 
 	@LocationId INT = NULL,
 	@StartDate DATETIME = NULL,
@@ -17,6 +24,15 @@ JOIN StriveCarSalon.tblLocation l on l.LocationId = tc.LocationId
 WHERE (l.LocationId = @LocationId OR @LocationId = 0) AND
 (tc.EventDate BETWEEN @StartDate AND @EndDate OR (@StartDate IS NULL AND @EndDate IS NULL))
 AND ISNULL(tc.IsDeleted,0) = 0 AND tc.IsActive = 1
+AND ISNULL(e.IsDeleted,0) = 0 AND e.IsActive = 1
 
-
+Select distinct 
+EmployeeId, 
+CONCAT(FirstName,' ',LastName) AS EmployeeName
+FROM StriveCarSalon.tblEmployee 
+WHERE 
+EmployeeId 
+NOT IN
+(SELECT EmployeeId FROM tblTimeClock where EventDate BETWEEN @StartDate AND @EndDate 
+AND LocationId=@LocationId AND IsActive=1 AND ISNULL(IsDeleted,0)=0)AND IsActive=1 AND ISNULL(IsDeleted,0)=0
 END
