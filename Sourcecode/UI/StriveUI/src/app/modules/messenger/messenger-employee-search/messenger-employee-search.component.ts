93ffacf807fa410e0d66b1a009579641b5770ba1
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-10-07 01:44:06 -0400

    integrating getAllEmp in messanger

--- Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger-employee-search/messenger-employee-search.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger-employee-search/messenger-employee-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/services/data-service/employee.service';
 declare var $: any;
 @Component({
   selector: 'app-messenger-employee-search',
@@ -6,17 +7,68 @@ declare var $: any;
   styleUrls: ['./messenger-employee-search.component.css']
 })
 export class MessengerEmployeeSearchComponent implements OnInit {
-
-  constructor() { }
+  search = '';
+  empList = [];
+  selectAll = false;
+  constructor(private empService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.getAllEmployees();
   }
-  closeemp(){
+  closeemp() {
     $('#show-search-emp').hide();
     $('.internal-employee').addClass('col-xl-9');
     $('.internal-employee').removeClass('col-xl-6');
     $('.view-msg').addClass('Message-box-slide');
     $('.view-msg').removeClass('Message-box');
     $('.plus-icon').removeClass('opacity-16');
+    this.clearValue();
+  }
+  clearValue() {
+    this.search = '';
+    this.clearSelectAllFlag();
+    this.getAllEmployees();
+  }
+  clearSelectAllFlag() {
+    (document.getElementById('selectAll') as HTMLInputElement).checked = false;
+  }
+  getAllEmployees() {
+    this.clearSelectAllFlag();
+    this.empService.searchEmployee(this.search).subscribe(data => {
+      if (data.status === 'Success') {
+        const empList = JSON.parse(data.resultData);
+        this.empList = empList.EmployeeList;
+        this.setDefaultBoolean(false);
+        this.setName();
+      }
+    });
+  }
+  setName() {
+    this.empList.map(item => {
+      const intial = item.FirstName.charAt(0).toUpperCase() + item.LastName.charAt(0).toUpperCase();
+      console.log(intial);
+      item.Initial = intial;
+    });
+  }
+  setDefaultBoolean(flag) {
+    this.empList.forEach(item => {
+      item.isSelected = flag;
+    });
+  }
+  selectAllEmployees(event) {
+    if (event.target.checked === true) {
+      this.setDefaultBoolean(true);
+    } else {
+      this.setDefaultBoolean(false);
+    }
+  }
+  addEmployees() {
+    const selectedEmp = this.empList.filter(item => item.isSelected === true);
+  }
+  searchFocus() {
+    this.search = this.search.trim();
+  }
+  changeState() {
+    this.clearSelectAllFlag();
   }
 }
--- Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger.module.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger.module.ts
@@ -5,13 +5,15 @@ import { MessengerRoutingModule } from './messenger-routing.module';
 import { MessengerComponent } from './messenger.component';
 import { MessengerEmployeeListComponent } from './messenger-employee-list/messenger-employee-list.component';
 import { MessengerEmployeeSearchComponent } from './messenger-employee-search/messenger-employee-search.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [MessengerComponent, MessengerEmployeeListComponent, MessengerEmployeeSearchComponent],
   imports: [
     CommonModule,
-    MessengerRoutingModule
+    MessengerRoutingModule,
+    SharedModule
   ]
 })
 export class MessengerModule { }
