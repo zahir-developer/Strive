93f1d6c312d8294114f41b7fda42de5ddec5d8cc
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-10-08 05:37:13 -0400

    sample json integratin done for messenger

--- Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger.component.ts
@@ -6,10 +6,21 @@ declare var $: any;
   styleUrls: ['./messenger.component.css']
 })
 export class MessengerComponent implements OnInit {
-
+  sampleJson = [];
+  message = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.sampleJson = [{ userId: 'me', FirstName: 'Mathu', LastName: 'Priya', body: 'hai', date: new Date() },
+    { userId: 'others', FirstName: 'karthi', LastName: 'sri', body: 'hw r u', date: new Date() },
+    { userId: 'me', FirstName: 'Mathu', LastName: 'Priya', body: 'gm', date: new Date() }];
+    this.setInitial();
+  }
+  setInitial() {
+    this.sampleJson.forEach(item => {
+      const intial = item.FirstName.charAt(0).toUpperCase() + item.LastName.charAt(0).toUpperCase();
+      item.Initial = intial;
+    });
   }
   openemp() {
     $('#show-search-emp').show();
@@ -18,5 +29,10 @@ export class MessengerComponent implements OnInit {
     $('.view-msg').removeClass('Message-box-slide');
     $('.view-msg').addClass('Message-box');
     $('.plus-icon').addClass('opacity-16');
+  }
+  sendMessage() {
+if (this.message !== '' && this.message !== undefined) {
+console.log(this.message);
 }
+  }
 }
