90ea5a40fea0131d16bdc90580a3d91bd2bff5a6
Ganesh <ganesh.kubendrababu@telliant.net>
2020-10-08 22:18:02 +0530

    PastDetails API Integrated and supported...

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Fragments/PastDetailsPageFragment.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.Android/StriveCustomer.Android/Fragments/PastDetailsPageFragment.cs
@@ -13,6 +13,7 @@ using Android.Widget;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.IoC;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
+using Strive.Core.Models.Customer;
 using Strive.Core.ViewModels.Customer;
 
 namespace StriveCustomer.Android.Fragments
@@ -20,6 +21,14 @@ namespace StriveCustomer.Android.Fragments
     [MvxUnconventionalAttribute]
     public class PastDetailsPageFragment : MvxFragment<PastDetailViewModel>
     {
+        PastClientDetails pastClient;
+        float TotalCost;
+        public PastDetailsPageFragment(PastClientDetails pastClient, float TotalCost)
+        {
+            this.pastClient = new PastClientDetails();
+            this.TotalCost = TotalCost;
+            this.pastClient = pastClient;
+        }
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -31,6 +40,19 @@ namespace StriveCustomer.Android.Fragments
         {
             var ignore = base.OnCreateView(inflater, container, savedInstanceState);
             var rootview = this.BindingInflate(Resource.Layout.PastDetailsPageFragment, null);
+            
+            
+            rootview.FindViewById<TextView>(Resource.Id.carModelName).Text = pastClient.Color + " " + pastClient.Make + " " + pastClient.Model;
+            rootview.FindViewById<TextView>(Resource.Id.barcodeNumber).Text = pastClient.Barcode;
+            rootview.FindViewById<TextView>(Resource.Id.carMake).Text = pastClient.Make;
+            rootview.FindViewById<TextView>(Resource.Id.carModel).Text = pastClient.Model;
+            rootview.FindViewById<TextView>(Resource.Id.carColor).Text = pastClient.Color;
+            var splits = pastClient.DetailVisitDate.Split('T');
+            rootview.FindViewById<TextView>(Resource.Id.carVisitDate).Text = splits[0];
+            rootview.FindViewById<TextView>(Resource.Id.packageName).Text = pastClient.ServiceName;
+            rootview.FindViewById<TextView>(Resource.Id.additionalService).Text = "Yes";
+            rootview.FindViewById<TextView>(Resource.Id.washPrice).Text = TotalCost.ToString();
+
             return rootview;
         }
     }
