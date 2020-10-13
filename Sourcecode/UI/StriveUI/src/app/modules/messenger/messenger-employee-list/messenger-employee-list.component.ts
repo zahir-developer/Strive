3715dfdf8548c9efdfc7f6102dad5b39abe8d8e2
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-10-13 12:22:29 -0400

    ui changes

--- Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger-employee-list/messenger-employee-list.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/messenger/messenger-employee-list/messenger-employee-list.component.ts
@@ -3,6 +3,7 @@ import { LocalStorage } from '@ng-idle/core';
 import { Subscription } from 'rxjs';
 import { ThemeService } from 'src/app/shared/common-service/theme.service';
 import { MessengerService } from 'src/app/shared/services/data-service/messenger.service';
+import { SignalRService } from 'src/app/shared/services/data-service/signal-r.service';
 
 @Component({
   selector: 'app-messenger-employee-list',
@@ -12,10 +13,15 @@ import { MessengerService } from 'src/app/shared/services/data-service/messenger
 export class MessengerEmployeeListComponent implements OnInit {
   search = '';
   empList = [];
+  empOnlineStatus: any;
   @Output() emitLoadMessageChat = new EventEmitter();
-  constructor(private msgService: MessengerService) { }
+  constructor(private msgService: MessengerService, private signalrService: SignalRService) { }
   ngOnInit(): void {
     this.getRecentChatHistory();
+    this.signalrService.communicationId.subscribe(data => {
+      this.empOnlineStatus = data;
+      this.getRecentChatHistory();
+    });
   }
   getRecentChatHistory() {
     this.msgService.GetEmployeeList().subscribe(data => {
@@ -23,18 +29,28 @@ export class MessengerEmployeeListComponent implements OnInit {
         const empList = JSON.parse(data.resultData);
         this.empList = empList?.EmployeeList?.ChatEmployeeList;
         this.setName();
+        this.setCommunicationId();
       }
     });
   }
+  setCommunicationId() {
+    if (this.empOnlineStatus !== undefined) {
+      this.empList.forEach(item => {
+        if (+item.EmployeeId === +this.empOnlineStatus[0]) {
+          item.communicationId = this.empOnlineStatus[1];
+        }
+      });
+    }
+  }
   setName() {
     if (this.empList.length > 0) {
-    this.empList.map(item => {
-      const intial = item.FirstName.charAt(0).toUpperCase() + item.LastName.charAt(0).toUpperCase();
-      item.Initial = intial;
-    });
-    this.emitLoadMessageChat.emit(this.empList[0]);
+      this.empList.map(item => {
+        const intial = item.FirstName.charAt(0).toUpperCase() + item.LastName.charAt(0).toUpperCase();
+        item.Initial = intial;
+      });
+      this.emitLoadMessageChat.emit(this.empList[0]);
+    }
   }
-}
   loadChat(employeeObj) {
     this.emitLoadMessageChat.emit(employeeObj);
   }
