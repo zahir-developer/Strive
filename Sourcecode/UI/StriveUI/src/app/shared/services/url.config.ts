7e0c7e226ab8d64fde07547cc394812cbb15e5ed
Ananth Subburaj <ananth.subburaj@telliant.net>
2020-10-13 19:31:34 +0530

    payroll grid api integrated

--- Sourcecode/UI/StriveUI/src/app/shared/services/url.config.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/services/url.config.ts
@@ -133,6 +133,7 @@ export const UrlConfig = {
     saveEmployeeWithService: `Admin/Details/AddServiceEmployee`,
     updateProductObj: `Admin/Sales/SaveProductItem`,
     getJobStatus: `Admin/Common/GetCodesByCategory/`,
-    getUncheckedVehicleDetails:`Admin/Checkout/GetUncheckedVehicleDetails`
+    getUncheckedVehicleDetails: `Admin/Checkout/GetUncheckedVehicleDetails`,
+    getPayroll: `Admin/PayRoll/GetPayroll`
   }
 };
