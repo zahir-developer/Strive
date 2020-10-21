a5da29d1985888c8eddbac9cedf9fb0f413fce1a
Ananth Subburaj <ananth.subburaj@telliant.net>
2020-10-21 12:39:21 +0530

    edit changes

--- Sourcecode/UI/StriveUI/src/app/modules/payrolls/payrolls-grid/payrolls-grid.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/payrolls/payrolls-grid/payrolls-grid.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { PayrollsService } from 'src/app/shared/services/data-service/payrolls.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-payrolls-grid',
@@ -15,6 +16,7 @@ export class PayrollsGridComponent implements OnInit {
   pageSize = 5;
   collectionSize = 0;
   isEditAdjustment: boolean;
+  @ViewChild('content') content: ElementRef;
   constructor(
     private payrollsService: PayrollsService,
     private fb: FormBuilder,
@@ -50,10 +52,10 @@ export class PayrollsGridComponent implements OnInit {
     const endDate = this.datePipe.transform(this.payrollDateForm.value.toDate, 'yyyy-MM-dd');
     this.payrollsService.getPayroll(48, '2020-09-27', '2020-09-28').subscribe(res => {
       if (res.status === 'Success') {
-        const payRoll = JSON.parse(res.resultData); 
+        const payRoll = JSON.parse(res.resultData);
         if (payRoll.Result.PayRollRateViewModel !== null) {
           this.payRollList = payRoll.Result.PayRollRateViewModel;
-          this.payRollList.forEach( item => {
+          this.payRollList.forEach(item => {
             item.isEditAdjustment = false;
           });
           this.collectionSize = Math.ceil(this.payRollList.length / this.pageSize) * 10;
@@ -63,19 +65,39 @@ export class PayrollsGridComponent implements OnInit {
   }
 
   editAdjustemt() {
-    this.isEditAdjustment = !this.isEditAdjustment;
+    this.isEditAdjustment = true;
+    this.payrollDateForm.disable();
   }
 
-  cancelEditAdjusment(payroll) {
-    this.payRollList.forEach( item => {
-      if (payroll.EmployeeId === item.EmployeeId ) {
-        item.isEditAdjustment = false;
-      }
-    });
+  CancelAdjustemt() {
+    this.isEditAdjustment = false;
+    this.payrollDateForm.enable();
   }
 
   saveAdjustemt() {
     this.runReport();
+  }
+
+  updateAdjustment() {
+    console.log(this.payRollList, 'edit');
+  }
+
+  processPayrolls() {
+    // const doc = new jsPDF();
+    // doc.text('Hello world!', 1, 1);
+    // doc.save('two-by-four.pdf');
+    // doc.addHTML(this.content.nativeElement, function() {
+    //    doc.save("obrz.pdf");
+    // });
+    // console.log(this.content.nativeElement);
+    // doc.addHTML(this.content.nativeElement, function () {
+    //   doc.save('obrz.pdf');
+    // });
+    const elementToPrint = document.getElementById('content'); // The html element to become a pdf
+    const pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.addHTML(elementToPrint, () => {
+      pdf.save('web.pdf');
+    });
   }
 
 }
