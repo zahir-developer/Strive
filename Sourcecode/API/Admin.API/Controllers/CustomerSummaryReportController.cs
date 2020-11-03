1e1dcb89d40bb61efd8a9d00cdba3309f8332fa4
Arunkumar S <arun.suriyanarayanan@telliant.net>
2020-11-03 15:26:58 +0530

    corrections

--- Sourcecode/API/Admin.API/Controllers/CustomerSummaryReportController.cs
+++ Sourcecode/API/Admin.API/Controllers/CustomerSummaryReportController.cs
@@ -1,39 +1,39 @@
-﻿using Admin.API.Helpers;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Strive.BusinessEntities.DTO.CustomerMonthlyDetailedReport;
-using Strive.BusinessEntities.DTO.CustomerSummaryReport;
-using Strive.BusinessLogic.CustomerSummaryReport;
-using Strive.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Admin.API.Helpers;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Strive.BusinessEntities.DTO.CustomerMonthlyDetailedReport;
+////using Strive.BusinessEntities.DTO.CustomerSummaryReport;
+//using Strive.BusinessLogic.CustomerSummaryReport;
+//using Strive.Common;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Admin.API.Controllers
-{
-    [Authorize]
+//namespace Admin.API.Controllers
+//{
+//    [Authorize]
 
-    [Route("Admin/[Controller]")]
-    public class CustomerSummaryReportController : StriveControllerBase<ICustomerSummaryReportBpl>
-    {
-        public CustomerSummaryReportController(ICustomerSummaryReportBpl msBpl) : base(msBpl) { }
-        #region
-        /// <summary>
-        /// Method to Get Customer Summary Report.
-        /// </summary>
-        [HttpPost]
-        [Route("GetCustomerSummaryReport")]
-        public Result GetCustomerSummaryReport([FromBody] CustomerSummaryReportDto customersummary) => _bplManager.GetCustomerSummaryReport(customersummary);
+//    [Route("Admin/[Controller]")]
+//    public class CustomerSummaryReportController : StriveControllerBase<ICustomerSummaryReportBpl>
+//    {
+//        public CustomerSummaryReportController(ICustomerSummaryReportBpl msBpl) : base(msBpl) { }
+//        #region
+//        /// <summary>
+//        /// Method to Get Customer Summary Report.
+//        /// </summary>
+//        //[HttpPost]
+//        //[Route("GetCustomerSummaryReport")]
+//        //public Result GetCustomerSummaryReport([FromBody] CustomerSummaryReportDto customersummary) => _bplManager.GetCustomerSummaryReport(customersummary);
 
-        [HttpPost]
-        [Route("GetCustomerMonthlyDetailed")]
-        public Result GetCustomerMonthlyDetailed([FromBody] CustomerSummaryReportDto customersummary) => _bplManager.GetCustomerSummaryReport(customersummary);
+//        //[HttpPost]
+//        //[Route("GetCustomerMonthlyDetailed")]
+//        //public Result GetCustomerMonthlyDetailed([FromBody] CustomerSummaryReportDto customersummary) => _bplManager.GetCustomerSummaryReport(customersummary);
 
-        [HttpPost]
-        [Route("GetCustomerMonthlyDetailedReport")]
-        public Result GetCustomerMonthlyDetailedReport([FromBody] CustomerMonthlyDetailedReport customerMonthlyDetail) => _bplManager.GetCustomerMonthlyDetailedReport(customerMonthlyDetail);
+//        [HttpPost]
+//        [Route("GetCustomerMonthlyDetailedReport")]
+//        public Result GetCustomerMonthlyDetailedReport([FromBody] CustomerMonthlyDetailedReport customerMonthlyDetail) => _bplManager.GetCustomerMonthlyDetailedReport(customerMonthlyDetail);
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
--- Sourcecode/API/Strive.BusinessLogic/CustomerSummaryReport/CustomerSummaryReportBpl.cs
+++ Sourcecode/API/Strive.BusinessLogic/CustomerSummaryReport/CustomerSummaryReportBpl.cs
@@ -1,28 +1,28 @@
-﻿using Microsoft.Extensions.Caching.Distributed;
-using Strive.BusinessEntities.DTO.CustomerMonthlyDetailedReport;
-using Strive.BusinessEntities.DTO.CustomerSummaryReport;
-using Strive.Common;
-using Strive.ResourceAccess;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.Extensions.Caching.Distributed;
+//using Strive.BusinessEntities.DTO.CustomerMonthlyDetailedReport;
+////using Strive.BusinessEntities.DTO.CustomerSummaryReport;
+//using Strive.Common;
+//using Strive.ResourceAccess;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Strive.BusinessLogic.CustomerSummaryReport
-{
-    public class CustomerSummaryReportBpl : Strivebase, ICustomerSummaryReportBpl
-    {
-        public CustomerSummaryReportBpl(IDistributedCache cache, ITenantHelper tenantHelper) : base(tenantHelper, cache)
-    {
-    }
-    public Result GetCustomerSummaryReport(CustomerSummaryReportDto customersummary)
-    {
-        return ResultWrap(new CustomerSummaryReportRal(_tenant).GetCustomerSummaryReport, customersummary, "GetCustomerSummaryReport");
-    }
-        public Result GetCustomerMonthlyDetailedReport(CustomerMonthlyDetailedReport customerMonthlyDetail)
-        {
-            return ResultWrap(new CustomerSummaryReportRal(_tenant).GetCustomerMonthlyDetailReport, customerMonthlyDetail, "GetCustomerMonthlyDetailReport");
-        }
-    }
-}
+//namespace Strive.BusinessLogic.CustomerSummaryReport
+//{
+//    public class CustomerSummaryReportBpl : Strivebase, ICustomerSummaryReportBpl
+//    {
+//        public CustomerSummaryReportBpl(IDistributedCache cache, ITenantHelper tenantHelper) : base(tenantHelper, cache)
+//    {
+//    }
+//    //public Result GetCustomerSummaryReport(CustomerSummaryReportDto customersummary)
+//    //{
+//    //    return ResultWrap(new CustomerSummaryReportRal(_tenant).GetCustomerSummaryReport, customersummary, "GetCustomerSummaryReport");
+//    //}
+//        //public Result GetCustomerMonthlyDetailedReport(CustomerMonthlyDetailedReport customerMonthlyDetail)
+//        //{
+//        //    return ResultWrap(new CustomerSummaryReportRal(_tenant).GetCustomerMonthlyDetailReport, customerMonthlyDetail, "GetCustomerMonthlyDetailReport");
+//        //}
+//    }
+//}
