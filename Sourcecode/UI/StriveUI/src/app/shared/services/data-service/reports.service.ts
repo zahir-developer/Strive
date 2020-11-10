1b85e8f99cd77a0e2732426e720a550e9202b700
Ragavi Saravanan <ragavi.saravanan@telliant.net>
2020-11-10 07:18:26 -0500

    daily status ui changes and totalprice property added in API/UI

--- Sourcecode/UI/StriveUI/src/app/shared/services/data-service/reports.service.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/services/data-service/reports.service.ts
@@ -25,4 +25,7 @@ export class ReportsService {
   getMonthlyTipReport(obj): Observable<any> {
     return this.http.post(`${UrlConfig.reports.getMonthlyTipReport}`, obj);
   }
+  getDailyStatusReport(obj): Observable<any> {
+    return this.http.post(`${UrlConfig.reports.getDailyStatusReport}`, obj);
+  }
 }
