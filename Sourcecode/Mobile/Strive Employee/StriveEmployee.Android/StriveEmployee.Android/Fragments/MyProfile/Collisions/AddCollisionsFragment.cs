6bf46fdbf7017d5a22d422c7f6f6c5d936a231c2
Ganesh <ganesh.kubendrababu@telliant.net>
2020-11-19 19:55:21 +0530

    Added Permissions ...

--- Sourcecode/Mobile/Strive Employee/StriveEmployee.Android/StriveEmployee.Android/Fragments/MyProfile/Collisions/AddCollisionsFragment.cs
+++ Sourcecode/Mobile/Strive Employee/StriveEmployee.Android/StriveEmployee.Android/Fragments/MyProfile/Collisions/AddCollisionsFragment.cs
@@ -8,6 +8,7 @@ using Android.Content;
 using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
@@ -20,6 +21,7 @@ namespace StriveEmployee.Android.Fragments.MyProfile.Collisions
     public class AddCollisionsFragment : MvxFragment<AddCollisionsViewModel>
     {
         private TextView addImage_TextView;
+        private AddCollisions_DialogFragment collisions_DialogFragment;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -34,8 +36,15 @@ namespace StriveEmployee.Android.Fragments.MyProfile.Collisions
 
             addImage_TextView = rootView.FindViewById<TextView>(Resource.Id.addImage_TextView);
             addImage_TextView.PaintFlags = PaintFlags.UnderlineText;
-
+            addImage_TextView.Click += AddImage_TextView_Click;
             return rootView;
+        }
+
+        private void AddImage_TextView_Click(object sender, EventArgs e)
+        {
+            AppCompatActivity activity = (AppCompatActivity)this.Context;
+            collisions_DialogFragment = new AddCollisions_DialogFragment();
+            collisions_DialogFragment.Show(activity.SupportFragmentManager,"AddCollisions_DialogFragment");
         }
     }
 }
---
+++ Sourcecode/Mobile/Strive Employee/StriveEmployee.Android/StriveEmployee.Android/Fragments/MyProfile/Collisions/AddCollisions_DialogFragment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Content;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using StriveEmployee.Android.Resources.Services;
+
+namespace StriveEmployee.Android.Fragments.MyProfile.Collisions
+{    
+    public class AddCollisions_DialogFragment : BottomSheetDialogFragment
+    {
+        private ImageView openGallery_ImageView;
+        private ImageView openCamera_ImageView;
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.AddCollisions_BottomSheet, container, false);
+
+            openGallery_ImageView = view.FindViewById<ImageView>(Resource.Id.openGallery_ImageView);
+            openCamera_ImageView = view.FindViewById<ImageView>(Resource.Id.openCamera_ImageView);
+
+            openGallery_ImageView.Click += OpenGallery_ImageView_Click;
+            openCamera_ImageView.Click += OpenCamera_ImageView_Click;
+
+
+
+            return view;
+        }
+
+        private void OpenCamera_ImageView_Click(object sender, EventArgs e)
+        {
+            TakePhoto();
+        }
+
+        private async void OpenGallery_ImageView_Click(object sender, EventArgs e)
+        {
+            UploadPhoto();
+        }
+
+        private async void TakePhoto()
+        {
+            await Permission_Services.checkCameraPermission(this);
+            await Permission_Services.ReadExternalStoragePermission(this);
+            await Permission_Services.WriteExternalStoragePermission(this);
+
+            if (ContextCompat.CheckSelfPermission(Context, Manifest.Permission.Camera) == Permission.Granted &&
+                ContextCompat.CheckSelfPermission(Context, Manifest.Permission.ReadExternalStorage) == Permission.Granted &&
+                ContextCompat.CheckSelfPermission(Context, Manifest.Permission.WriteExternalStorage) == Permission.Granted)
+            {
+
+            }
+        }
+
+        private async void UploadPhoto()
+        {
+          
+        }
+    }
+}
