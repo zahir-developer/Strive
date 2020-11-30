391871dc7ab8fba969a3652c4874c104cbe4be3b
BhavaniThamizh <bhavani.annadurai@telliant.net>
2020-12-01 01:16:01 +0530

    Pickerview in AddVehicle and functionality completed.

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/AddVehicleList.designer.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/AddVehicleList.designer.cs
@@ -1,21 +1,107 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace StriveCustomer.iOS.Views
 {
-    [Register("AddVehicleList")]
-    partial class AddVehicleList
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("AddVehicleList")]
+	partial class AddVehicleList
+	{
+		[Outlet]
+		UIKit.UIButton SaveAddVehicle_Btn { get; set; }
+
+		[Outlet]
+		UIKit.UIButton SelectMembership_Text { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView VehicleColor_Arrow { get; set; }
+
+		[Outlet]
+		UIKit.UITextField VehicleColor_TextField { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView VehicleMake_Arrow { get; set; }
+
+		[Outlet]
+		UIKit.UITextField VehicleMake_TextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField VehicleModel_TextField { get; set; }
+
+		[Action ("Make_TouchInside:")]
+		partial void Make_TouchInside (UIKit.UITextField sender);
+
+		[Action ("Make_TouchOutside:")]
+		partial void Make_TouchOutside (UIKit.UITextField sender);
+
+		[Action ("Model_TouchInside:")]
+		partial void Model_TouchInside (UIKit.UITextField sender);
+
+		[Action ("SaveAddVehicle_BtnTouch:")]
+		partial void SaveAddVehicle_BtnTouch (UIKit.UIButton sender);
+
+		[Action ("SelectMembership_Touch:")]
+		partial void SelectMembership_Touch (UIKit.UIButton sender);
+
+		[Action ("VehicleColor_SpinnerTouchBegin:")]
+		partial void VehicleColor_SpinnerTouchBegin (UIKit.UITextField sender);
+
+		[Action ("VehicleColor_SpinnerTouchEnd:")]
+		partial void VehicleColor_SpinnerTouchEnd (UIKit.UITextField sender);
+
+		[Action ("VehicleMake_SpinnerTouchBegin:")]
+		partial void VehicleMake_SpinnerTouchBegin (UIKit.UITextField sender);
+
+		[Action ("VehicleMake_SpinnerTouchEnd:")]
+		partial void VehicleMake_SpinnerTouchEnd (UIKit.UITextField sender);
+
+		[Action ("VehicleModel_SpinnerTouchBegin:")]
+		partial void VehicleModel_SpinnerTouchBegin (UIKit.UITextField sender);
+
+		[Action ("VehicleModel_SpinnerTouchEnd:")]
+		partial void VehicleModel_SpinnerTouchEnd (UIKit.UITextField sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (VehicleColor_Arrow != null) {
+				VehicleColor_Arrow.Dispose ();
+				VehicleColor_Arrow = null;
+			}
+
+			if (VehicleColor_TextField != null) {
+				VehicleColor_TextField.Dispose ();
+				VehicleColor_TextField = null;
+			}
+
+			if (VehicleMake_Arrow != null) {
+				VehicleMake_Arrow.Dispose ();
+				VehicleMake_Arrow = null;
+			}
+
+			if (VehicleMake_TextField != null) {
+				VehicleMake_TextField.Dispose ();
+				VehicleMake_TextField = null;
+			}
+
+			if (VehicleModel_TextField != null) {
+				VehicleModel_TextField.Dispose ();
+				VehicleModel_TextField = null;
+			}
+
+			if (SelectMembership_Text != null) {
+				SelectMembership_Text.Dispose ();
+				SelectMembership_Text = null;
+			}
+
+			if (SaveAddVehicle_Btn != null) {
+				SaveAddVehicle_Btn.Dispose ();
+				SaveAddVehicle_Btn = null;
+			}
+		}
+	}
 }
---
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/VehicleMakePickerSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace StriveCustomer.iOS.Views
+{
+    public class VehicleMakePickerSource : UIPickerViewModel
+    {
+        List<string> makeList;
+        UITextField textValue;
+        public string SelectedValue;
+        public EventHandler ValueChanged;
+        public VehicleMakePickerSource(List<string> sourceList, UITextField textField)
+        {
+            this.makeList = sourceList;
+            this.textValue = textField;
+        }
+
+        public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+        {
+            return makeList.Count;
+        }
+        public override nint GetComponentCount(UIPickerView pickerView)
+        {
+            return 1;
+        }
+        public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+        {
+            return makeList[(int)row];
+        }
+        public override void Selected(UIPickerView pickerView, nint row, nint component)
+        {
+            var selectedItem = makeList[(int)row];
+            SelectedValue = selectedItem;
+            ValueChanged?.Invoke(null, null);
+            textValue.Text = SelectedValue;
+        }        
+    }
+}
