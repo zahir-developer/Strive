2ee71cf8c96156d0f240bc9a638ce5e695a0c41b
Ganesh <ganesh.kubendrababu@telliant.net>
2020-12-01 17:54:20 +0530

    Initial files to go along with android ..

---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/MainApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Acr.UserDialogs;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platforms.Android;
+using StriveOwner.Android.MvvmCross;
+
+namespace StriveOwner.Android
+{
+    [Application(UsesCleartextTraffic = true)]
+    public class MainApplication : MvxAppCompatApplication<Setup, App>
+    {
+        public MainApplication()
+        {
+        }
+
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+            UserDialogs.Init(() => Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity);
+        }
+    }
+}
---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/Views/LoginView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using Strive.Core.ViewModels.Owner;
+
+namespace StriveOwner.Android.Views
+{
+    [MvxActivityPresentation]
+    [Activity(Label = "Login View")]
+    public class LoginView : MvxAppCompatActivity<LoginViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+    }
+}
