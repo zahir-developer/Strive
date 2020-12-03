81bd705682404f4f5d819d9085903d1e66a081a5
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2020-12-03 19:37:38 +0530

    washhoursviewmodel updated

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/WashHoursViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/WashHoursViewModel.cs
@@ -9,6 +9,7 @@ namespace Strive.BusinessEntities.ViewModel
    public class WashHoursViewModel
     {
         public int JobId { get; set; }
+        public DateTime JobDate { get; set; }
         public int _6AM { get; set; }
         public int _7AM { get; set; }
         public int _8AM { get; set; }
@@ -22,5 +23,8 @@ namespace Strive.BusinessEntities.ViewModel
         public int _4PM { get; set; }
         public int _5PM { get; set; }
         public int _6PM { get; set; }
+        public int _7PM { get; set; }
+        public int _8PM { get; set; }
+        public int _9PM { get; set; }
     }
 }
