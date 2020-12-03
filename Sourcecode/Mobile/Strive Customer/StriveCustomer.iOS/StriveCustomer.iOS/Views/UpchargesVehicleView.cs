bc8afdf4e0f4ea14aa7a27fe16f34f87373ad96f
BhavaniThamizh <bhavani.annadurai@telliant.net>
2020-12-03 23:38:12 +0530

    Membership and Upcharges on Vehicle list completed.

---
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/UpchargesVehicleView.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using CoreGraphics;
+using MvvmCross.Platforms.Ios.Views;
+using Strive.Core.Models.Customer;
+using Strive.Core.Models.TimInventory;
+using Strive.Core.ViewModels.Customer;
+using UIKit;
+
+namespace StriveCustomer.iOS.Views
+{
+    public partial class UpchargesVehicleView : MvxViewController<VehicleUpchargeViewModel>
+    {
+        public UpchargesVehicleView() : base("UpchargesVehicleView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            InitialSetup();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void InitialSetup()
+        {
+            var rightBtn = new UIButton(UIButtonType.Custom);
+            rightBtn.SetTitle("Next", UIControlState.Normal);
+            rightBtn.SetTitleColor(UIColor.FromRGB(0, 110, 202), UIControlState.Normal);
+
+            var rightBarBtn = new UIBarButtonItem(rightBtn);
+            NavigationItem.SetRightBarButtonItems(new UIBarButtonItem[] { rightBarBtn }, false);
+            rightBtn.TouchUpInside += (sender, e) =>
+            {
+                if (ViewModel.VehicleUpchargeCheck())
+                {
+                    
+                }
+
+            };
+
+            UpchargesVehicle_TableView.Layer.CornerRadius = 5;
+            UpchargesVehicle_TableView.RegisterNibForCellReuse(MembershipVehicle_ViewCell.Nib, MembershipVehicle_ViewCell.Key);
+            UpchargesVehicle_TableView.ReloadData();
+
+            getUpchargeList();
+        }
+
+        private async void getUpchargeList()
+        {
+            await this.ViewModel.getServiceList(MembershipDetails.selectedMembership);
+            await this.ViewModel.getAllServiceList();
+
+            if (MembershipDetails.filteredList != null)
+            {
+                var source = new UpchargesVehicleDataSource(MembershipDetails.filteredList);
+                UpchargesVehicle_TableView.Source = source;
+                UpchargesVehicle_TableView.TableFooterView = new UIView(CGRect.Empty);
+                UpchargesVehicle_TableView.DelaysContentTouches = false;
+                UpchargesVehicle_TableView.ReloadData();
+            }
+        }
+    }
+}
+
