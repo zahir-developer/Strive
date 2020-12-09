5a1254219e9dbea5caea4dfadeba1781d85d644d
vbalasubramanian <vineeth.balasubramanian@telliant.net>
2020-12-09 11:20:26 +0530

    UI and API Bug fix

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/BayJobDetailViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/BayJobDetailViewModel.cs
@@ -16,11 +16,11 @@ namespace Strive.BusinessEntities.ViewModel
         public string ClientName { get; set; }
         public string PhoneNumber { get; set; }
         public string EstimatedTimeOut { get; set; }
-        public string ServiceName { get; set; }
         public string ServiceTypeName { get; set; }
         public string VehicleMake { get; set; }
         public string VehicleModel { get; set; }
         public string VehicleColor { get; set; }
         public decimal Upcharge { get; set; }
+        public string OutsideService { get; set; }
     }
 }
