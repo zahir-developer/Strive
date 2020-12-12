33646980fb84d2eea39de5f0ce1eb362ce4d6b99
BhavaniThamizh <bhavani.annadurai@telliant.net>
2020-12-12 17:36:21 +0530

    Signature, Terms and conditions screen functionality completed.

--- Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/SignatureView.designer.cs
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/SignatureView.designer.cs
@@ -1,21 +1,56 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace StriveCustomer.iOS.Views
 {
-    [Register("SignatureView")]
-    partial class SignatureView
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("SignatureView")]
+	partial class SignatureView
+	{
+		[Outlet]
+		UIKit.UIButton CancelBtn_Sign { get; set; }
+
+		[Outlet]
+		UIKit.UIButton DoneBtn_Sign { get; set; }
+
+		[Outlet]
+		UIKit.UIView SignatureParentView { get; set; }
+
+		[Outlet]
+		Xamarin.Controls.SignaturePadView signPadView { get; set; }
+
+		[Action ("CancelBtn_SignTouch:")]
+		partial void CancelBtn_SignTouch (UIKit.UIButton sender);
+
+		[Action ("DoneBtn_SignTouch:")]
+		partial void DoneBtn_SignTouch (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (signPadView != null) {
+				signPadView.Dispose ();
+				signPadView = null;
+			}
+
+			if (CancelBtn_Sign != null) {
+				CancelBtn_Sign.Dispose ();
+				CancelBtn_Sign = null;
+			}
+
+			if (DoneBtn_Sign != null) {
+				DoneBtn_Sign.Dispose ();
+				DoneBtn_Sign = null;
+			}
+
+			if (SignatureParentView != null) {
+				SignatureParentView.Dispose ();
+				SignatureParentView = null;
+			}
+		}
+	}
 }
---
+++ Sourcecode/Mobile/Strive Customer/StriveCustomer.iOS/StriveCustomer.iOS/Views/TermsView.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Views;
+using Strive.Core.ViewModels.Customer;
+using StriveCustomer.iOS.UIUtils;
+using UIKit;
+
+namespace StriveCustomer.iOS.Views
+{
+    public partial class TermsView : MvxViewController<TermsAndConditionsViewModel>
+    {
+        public TermsView() : base("TermsView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            InitialSetup();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void InitialSetup()
+        {
+            var rightBtn = new UIButton(UIButtonType.Custom);
+            rightBtn.SetTitle("Next", UIControlState.Normal);
+            rightBtn.SetTitleColor(UIColor.FromRGB(0, 110, 202), UIControlState.Normal);
+
+            var rightBarBtn = new UIBarButtonItem(rightBtn);
+            NavigationItem.SetRightBarButtonItems(new UIBarButtonItem[] { rightBarBtn }, false);
+            rightBtn.TouchUpInside += (sender, e) =>
+            {
+                
+            };
+
+            NavigationController.NavigationBar.TitleTextAttributes = new UIStringAttributes()
+            {
+                Font = DesignUtils.OpenSansBoldFifteen(),
+                ForegroundColor = UIColor.Clear.FromHex(0x24489A),
+            };
+            NavigationItem.Title = "Vehicle";
+
+            TermsParentView.Layer.CornerRadius = 5;
+        }
+
+        partial void AgreeBtn_Touch(UIButton sender)
+        {
+            AgreeTerms();
+        }
+
+        partial void DisAgreeBtn_Touch(UIButton sender)
+        {
+            DisagreeMembership();
+        }
+
+        private async void AgreeTerms()
+        {
+            //CancelMembership
+            var result = await ViewModel.AgreeMembership();
+            if (result)
+            {                
+                SignatureClass.signaturePoints = null;
+                ViewModel.NavigateToLanding();
+            }
+        }
+
+        private async void DisagreeMembership()
+        {
+            var result = await ViewModel.DisagreeMembership();
+
+            if(result)
+            {
+                SignatureClass.signaturePoints = null;
+                ViewModel.NavigateToLanding();
+            }
+        }
+    }
+}
+
