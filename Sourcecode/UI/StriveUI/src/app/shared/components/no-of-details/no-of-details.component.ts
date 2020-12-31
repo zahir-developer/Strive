754a577c1e261e04b2cf4c881ce21cbc629c3e7c
Ananth Subburaj <ananth.subburaj@telliant.net>
2020-12-31 19:54:09 +0530

    add jobType property in the dashboard count api

--- Sourcecode/UI/StriveUI/src/app/shared/components/no-of-details/no-of-details.component.ts
+++ Sourcecode/UI/StriveUI/src/app/shared/components/no-of-details/no-of-details.component.ts
@@ -8,23 +8,40 @@ import { DetailService } from '../../services/data-service/detail.service';
 })
 export class NoOfDetailsComponent implements OnInit {
   detailCount: any;
-
+  jobTypeId: any;
   constructor( private detail: DetailService) { }
 
   ngOnInit() {
-    this.getDashboardDetails();
+    this.getJobType();
   }
 
   // Get Details Count
   getDashboardDetails() {
     const obj = {
       id: localStorage.getItem('empLocationId'),
-      date: new Date()
+      date: new Date(),
+      jobType: this.jobTypeId
     };
     this.detail.getDetailCount(obj).subscribe( res => {
       const wash = JSON.parse(res.resultData);
       if (wash.Dashboard !== null) {
         this.detailCount = wash.Dashboard.DetailsCount;
+      }
+    });
+  }
+
+  getJobType() {
+    this.detail.getJobType().subscribe(res => {
+      if (res.status === 'Success') {
+        const jobtype = JSON.parse(res.resultData);
+        if (jobtype.GetJobType.length > 0) {
+          jobtype.GetJobType.forEach(item => {
+            if (item.valuedesc === 'Detail') {
+              this.jobTypeId = item.valueid;
+              this.getDashboardDetails();
+            }
+          });
+        }
       }
     });
   }
