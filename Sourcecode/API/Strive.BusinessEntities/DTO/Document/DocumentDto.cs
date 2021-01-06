517235a7b6ec8d36219b0e792f4eb4c12c3ebd82
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-01-06 18:13:03 +0530

    documentlist reverted

--- Sourcecode/API/Strive.BusinessEntities/DTO/Document/DocumentDto.cs
+++ Sourcecode/API/Strive.BusinessEntities/DTO/Document/DocumentDto.cs
@@ -7,8 +7,8 @@ namespace Strive.BusinessEntities.Model
 {
     public class DocumentDto
     {
-        public List<Document> Document { get; set; }
+        public Document Document { get; set; }
 
-        public List<GlobalUpload.DocumentType> DocumentType { get; set; }
+        public GlobalUpload.DocumentType DocumentType { get; set; }
     }
 }
