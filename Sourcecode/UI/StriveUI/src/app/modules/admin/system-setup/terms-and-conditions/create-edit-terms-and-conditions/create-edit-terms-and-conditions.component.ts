5f3249eb81d82dfdfe9309b8c139f95195834e0b
Ananth Subburaj <ananth.subburaj@telliant.net>
2021-01-07 19:50:04 +0530

    remove set time out in term and condition document upload

--- Sourcecode/UI/StriveUI/src/app/modules/admin/system-setup/terms-and-conditions/create-edit-terms-and-conditions/create-edit-terms-and-conditions.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/admin/system-setup/terms-and-conditions/create-edit-terms-and-conditions/create-edit-terms-and-conditions.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { MessageServiceToastr } from 'src/app/shared/services/common-service/message.service';
 import { DocumentService } from 'src/app/shared/services/data-service/document.service';
 
@@ -10,18 +11,23 @@ import { DocumentService } from 'src/app/shared/services/data-service/document.s
 })
 export class CreateEditTermsAndConditionsComponent implements OnInit {
 
-  termsForm:FormGroup;
+  termsForm: FormGroup;
   @Output() closeDialog = new EventEmitter();
-  @Input() documentTypeId:any;
+  @Input() documentTypeId: any;
   fileName: any = null;
   isLoading: boolean;
   fileUploadformData: any = null;
   fileThumb: any = null;
   submitted: any;
   employeeId: any;
-  constructor(private fb:FormBuilder, private toastr: MessageServiceToastr, private document:DocumentService) { }
+  constructor(
+    private fb: FormBuilder,
+    private toastr: MessageServiceToastr,
+    private document: DocumentService,
+    private spinner: NgxSpinnerService
+  ) { }
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     if (localStorage.getItem('employeeName') !== undefined) {
       this.employeeId = +localStorage.getItem('empId');
     }
@@ -32,52 +38,47 @@ export class CreateEditTermsAndConditionsComponent implements OnInit {
     this.termsForm = this.fb.group({
       createdDate: [''],
       createdName: [''],
-      uploadBy:['',Validators.required]
-    }); 
+      uploadBy: ['', Validators.required]
+    });
   }
 
-  fileNameChanged() {
-    let filesSelected: any;
-    filesSelected = document.getElementById('customFile');
-    filesSelected = filesSelected.files;
-    if (filesSelected.length > 0) {
-      const fileToLoad = filesSelected[0];
-      this.fileName = fileToLoad.name;   
-      this.fileThumb = this.fileName.substring(this.fileName.lastIndexOf('.') + 1);
-      let fileReader: any;
-      fileReader = new FileReader();
-      fileReader.onload = function (fileLoadedEventTigger) {
-        let textAreaFileContents: any;
-        textAreaFileContents = document.getElementById('customFile');
-        textAreaFileContents.innerHTML = fileLoadedEventTigger.target.result;
-      };
-      fileReader.readAsDataURL(fileToLoad);
-      this.isLoading = true;
-      setTimeout(() => {
+  fileNameChanged(e: any) {
+    this.isLoading = true;
+    try {
+      const file = e.target.files[0];
+      const fReader = new FileReader();
+      fReader.readAsDataURL(file);
+      fReader.onloadend = (event: any) => {
+        console.log(file.name);
+        this.fileName = file.name;
+        const fileExtension = this.fileName.substring(this.fileName.lastIndexOf('.') + 1);
         let fileTosaveName: any;
-        if(this.fileThumb == 'PDF' || this.fileThumb == 'pdf'){
-          fileTosaveName = fileReader.result.split(',')[1];
-      }
-      else{
-        this.toastr.showMessage({ severity: 'error', title: 'Error', body: 'Upload Pdf Only' });
-        this.clearDocument();
-      }
+        fileTosaveName = event.target.result.split(',')[1];
         this.fileUploadformData = fileTosaveName;
+        const fileObj = {
+          fileName: this.fileName,
+          fileUploadDate: this.fileUploadformData,
+          fileType: fileExtension
+        };
         this.isLoading = false;
-        console.log(this.fileName,this.fileUploadformData.length);
-      }, 5000);
+      };
+    } catch (error) {
+      this.fileName = null;
+      this.fileUploadformData = null;
+      this.isLoading = false;
+      console.log('no file was selected...');
     }
   }
 
   clearDocument() {
     this.fileName = null;
-    this.fileThumb= null;
+    this.fileThumb = null;
     this.fileUploadformData = null;
   }
 
-  submit(){
-    this.submitted = true;    
-    if(this.fileName === null){   
+  submit() {
+    this.submitted = true;
+    if (this.fileName === null) {
       return;
     }
     const obj = {
@@ -96,11 +97,13 @@ export class CreateEditTermsAndConditionsComponent implements OnInit {
       updatedDate: new Date()
     };
     const finalObj = {
-      document:obj,
-      documentType:"TERMSANDCONDITION"
+      document: obj,
+      documentType: "TERMSANDCONDITION"
     };
+    this.spinner.show();
     this.document.addDocument(finalObj).subscribe(data => {
-      if (data.status === 'Success') {        
+      this.spinner.hide();
+      if (data.status === 'Success') {
         this.toastr.showMessage({ severity: 'success', title: 'Success', body: 'Document Saved Successfully' });
         this.closeDialog.emit({ isOpenPopup: false, status: 'saved' });
       } else {
