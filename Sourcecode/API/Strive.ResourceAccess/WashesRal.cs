154107bdf7e7a6cc5f854719ff5eaced285ec3ae
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-02-02 16:52:27 +0530

    params chnage

--- Sourcecode/API/Strive.ResourceAccess/WashesRal.cs
+++ Sourcecode/API/Strive.ResourceAccess/WashesRal.cs
@@ -23,7 +23,8 @@ namespace Strive.ResourceAccess
             _prm.Add("@Query", searchDto.Query);
             _prm.Add("@SortOrder", searchDto.SortOrder);
             _prm.Add("@SortBy", searchDto.SortBy);
-            _prm.Add("@Date", searchDto.Date);
+            _prm.Add("@StartDate", searchDto.StartDate);
+            _prm.Add("@EndDate", searchDto.EndDate);
 
             return db.FetchMultiResult<WashesListViewModel>(SPEnum.USPGETALLJOB.ToString(), _prm);
         }
