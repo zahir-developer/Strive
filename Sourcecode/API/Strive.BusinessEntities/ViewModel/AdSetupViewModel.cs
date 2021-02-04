29eec90acffc549c774cf540d79ef19a0d87cb78
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-02-04 19:42:55 +0530

    adsetup changes

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/AdSetupViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/AdSetupViewModel.cs
@@ -11,10 +11,6 @@ namespace Strive.BusinessEntities.ViewModel
         public int AdSetupId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
         public bool Status { get; set; }
-        public int? DocumentId { get; set; }
-
-        public string Base64 { get; set; }
     }
 }
