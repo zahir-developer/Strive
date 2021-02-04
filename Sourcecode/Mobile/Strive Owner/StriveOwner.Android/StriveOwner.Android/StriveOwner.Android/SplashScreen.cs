fb91be22ed814da295ab078ca3905ae896bb37d6
Ganesh <ganesh.kubendrababu@telliant.net>
2021-02-04 16:15:54 +0530

    Added all base files for the owner application..

---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace StriveOwner.Android
+{
+    [MvxActivityPresentation]
+    [Activity(
+      MainLauncher = true,
+      Icon = "@drawable/Strive_Logo_Splash",
+      Theme = "@style/SplashTheme",
+      NoHistory = true,
+      ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen()
+        {
+        }
+    }
+}
