5d87839a36e0b538ec4d7722d82ea0206e8a6f64
Ramya Iyappan <ramya.iyappan@telliant.net>
2021-02-19 12:12:14 +0530

    deal setup add

---
+++ Sourcecode/API/Strive.BusinessEntities/Model/DealSetup.cs
@@ -0,0 +1,32 @@
+﻿using Cocoon.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.Model
+{
+    [OverrideName("tbldeal")]
+    public class DealSetup
+    {
+        [Column, PrimaryKey, IgnoreOnInsert, IgnoreOnUpdate]
+
+
+        public int DealId { get; set; }
+
+        [Column]
+        public string DealName { get; set; }
+
+        [Column]
+        public string TimePeriod { get; set; }
+
+        [Column]
+        public bool Deals { get; set; }
+
+        [Column]
+        public DateTime StartDate { get; set; }
+        [Column]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Sourcecode/API/Strive.BusinessLogic/DealSetup/DealSetupBpl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Strive.BusinessEntities.DTO;
+using Strive.Common;
+using Strive.ResourceAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessLogic.DealSetup
+{
+    public class DealSetupBpl : Strivebase, IdealSetupBpl
+    {
+        public DealSetupBpl(IDistributedCache cache, ITenantHelper tenantHelper) : base(tenantHelper, cache) { }
+
+        public Result AddDealSetup(DealSetupDto dealSetup)
+        {
+            return ResultWrap(new DealSetupRal(_tenant).AddDealSetup, dealSetup, "dealSetup");
+        }
+    }
+}
