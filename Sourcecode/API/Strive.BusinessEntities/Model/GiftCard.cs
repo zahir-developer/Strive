1af7e61557405ac212102c7db00404ec752aa630
Ramya Iyappan <ramya.iyappan@telliant.net>
2021-03-02 20:33:23 +0530

    activation date change

--- Sourcecode/API/Strive.BusinessEntities/Model/GiftCard.cs
+++ Sourcecode/API/Strive.BusinessEntities/Model/GiftCard.cs
@@ -23,7 +23,7 @@ public class GiftCard
 	public string GiftCardName { get; set; }
 
 	[Column]
-	public DateTimeOffset? ExpiryDate { get; set; }
+	public DateTimeOffset? ActivationDate { get; set; }
     [Column]
     public decimal TotalAmount { get; set; }
     [Column]
