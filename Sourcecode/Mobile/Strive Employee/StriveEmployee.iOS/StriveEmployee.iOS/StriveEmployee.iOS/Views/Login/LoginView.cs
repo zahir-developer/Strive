86723d534dc84103d549a5232b56cfff4160303e
BhavaniThamizh <bhavani.annadurai@telliant.net>
2021-03-09 14:26:42 +0530

    Initial commit on Employee app setup

---
+++ Sourcecode/Mobile/Strive Employee/StriveEmployee.iOS/StriveEmployee.iOS/StriveEmployee.iOS/Views/Login/LoginView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Platforms.Ios.Views;
+using Strive.Core.ViewModels.Employee;
+using UIKit;
+
+namespace StriveEmployee.iOS.Views.Login
+{
+    public partial class LoginView :MvxViewController<LoginViewModel>
+    {
+        NSUserDefaults Persistance;
+        string UsernameKey = "username";
+        string PasswordKey = "password";
+
+        public LoginView() : base("LoginView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();      
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
