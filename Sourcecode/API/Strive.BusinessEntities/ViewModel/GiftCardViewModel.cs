3fad58621a3401634b35dd4d975b3fc232d35b00
Ramya Iyappan <ramya.iyappan@telliant.net>
2021-03-11 17:57:58 +0530

    first name and last name

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/GiftCardViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/GiftCardViewModel.cs
@@ -13,9 +13,11 @@ namespace Strive.BusinessEntities.ViewModel
         public string GiftCardCode { get; set; }
         public string GiftCardName { get; set; }
         public DateTimeOffset? ActivationDate { get; set; }
-        public decimal TotalAmount { get; set; }
+        public decimal? TotalAmount { get; set; }
         public string Comments { get; set; }
         public bool IsActive { get; set; }
         public bool IsDeleted { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
