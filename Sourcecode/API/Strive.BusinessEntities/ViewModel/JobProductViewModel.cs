f88a22575b04e41d684d029916076ecd19f5331d
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-03-12 14:07:49 +0530

    jobproductitem dto added

---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/JobProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Cocoon.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class JobProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        
+        public int Quantity { get; set; }
+    }
+}
