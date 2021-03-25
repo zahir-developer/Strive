b1be082a5b1460dbadbc8a99c55c41885379ea81
Ananth Subburaj <ananth.subburaj@telliant.net>
2021-03-25 19:12:49 +0530

    GetAllLocationWashTime Integration - UI

--- Sourcecode/UI/StriveUI/src/app/modules/dashboard/dashboard.component.ts
+++ Sourcecode/UI/StriveUI/src/app/modules/dashboard/dashboard.component.ts
@@ -160,10 +160,11 @@ export class DashboardComponent implements OnInit {
 
   // Get All Location
   getLocationList() {
-    this.dashboardService.getLocation().subscribe(res => {
+    const locID = 0;
+    this.dashboardService.getAllLocationWashTime(locID).subscribe(res => {
             if (res.status === 'Success') {
         const location = JSON.parse(res.resultData);
-        this.location = location.Location;
+        this.location = location.Washes;
       } else {
         this.messageService.showMessage({ severity: 'error', title: 'Error', body: 'Communication Error' });
       }
