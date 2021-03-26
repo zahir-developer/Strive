ecfd9fd50995f12e94ee779928e364857a52e16a
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-03-26 15:45:37 +0530

    giftcardhistory added in giftcardadd

--- Sourcecode/API/Strive.BusinessEntities/DTO/GiftCard/GiftCardDto.cs
+++ Sourcecode/API/Strive.BusinessEntities/DTO/GiftCard/GiftCardDto.cs
@@ -9,5 +9,6 @@ namespace Strive.BusinessEntities.DTO.GiftCard
     public class GiftCardDto
     {
         public Model.GiftCard GiftCard { get; set; }
+        public List<GiftCardHistoryDto> GiftCardHistory { get; set; }
     }
 }
