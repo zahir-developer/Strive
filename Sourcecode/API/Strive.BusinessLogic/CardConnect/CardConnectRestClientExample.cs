10b7d233370177356123bcad1b353c8498480d99
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2021-03-31 14:00:00 +0530

    CardConnect Payment Example

--- Sourcecode/API/Strive.BusinessLogic/CardConnect/CardConnectRestClientExample.cs
+++ Sourcecode/API/Strive.BusinessLogic/CardConnect/CardConnectRestClientExample.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json.Linq;
 
-namespace CardConnectRestClientExample
+namespace Strive.BusinessLogic
 {
     class CardConnectRestClientExample
     {
