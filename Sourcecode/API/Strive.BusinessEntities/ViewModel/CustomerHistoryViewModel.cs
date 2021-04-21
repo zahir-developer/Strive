91c485a765fe1f1b47c2913a6e13a6dcb8923db0
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-04-21 19:12:25 +0530

    customer history

---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/CustomerHistoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class CustomerHistoryViewModel
+    {
+        public int JobId { get; set; }
+        public DateTime ServiceDate { get; set; }
+        public string TicketNumber { get; set; }
+        public string ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string VehicleId { get; set; }
+        public string MembershipName { get; set; }
+        public string BarCode { get; set; }
+        public string AdditionalServices { get; set; }
+        public string Services { get; set; 
+    }
+}
