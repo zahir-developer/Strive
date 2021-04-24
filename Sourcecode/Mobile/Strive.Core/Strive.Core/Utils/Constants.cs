fa36319f24bc419b6cbce26173d4de4fd564ecac
Ganesh <ganesh.kubendrababu@telliant.net>
2021-04-24 19:05:18 +0530

    Added Fixes to Customer  stabilize Part - 1

--- Sourcecode/Mobile/Strive.Core/Strive.Core/Utils/Constants.cs
+++ Sourcecode/Mobile/Strive.Core/Strive.Core/Utils/Constants.cs
@@ -55,6 +55,8 @@ namespace Strive.Core.Utils
 
         public const string URL_GET_CLIENT_VEHICLE_SERVICES = "/Admin/ServiceSetup/GetService";
 
+        public const string URL_GET_CLIENT_VEHICLE_SERVICES_LIST = "/Admin/ServiceSetup/GetAllServiceDetail";
+
         public const string URL_GET_SELECTED_MEMBERSHIP_SERVICES = "/Admin/MembershipSetup/GetAllMembershipById/";  
 
         public const string URL_GET_CLIENT_BY_ID = "/Admin/Client/GetClientById/{0}";
@@ -107,7 +109,7 @@ namespace Strive.Core.Utils
 
         public const string URL_SCHEDULE_PAST_SERVICE = "/Admin/Details/GetAllDetails";
 
-        public const string URL_SCHEDULE_SERVICES_AVAILABLE = "/Admin/Sales/GetServicesWithPrice";
+        public const string URL_SCHEDULE_SERVICES_AVAILABLE = "/Admin/Sales/GetAllServiceDetail";
 
         public const string URL_SCHEDULE_TIME_SLOTS = "/Admin/Dashboard/GetAvailablilityScheduleTime";
 
