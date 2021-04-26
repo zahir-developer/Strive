237dcd60f4b7ff4b6f05cd0629c6408be8ee3047
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-04-26 20:11:53 +0530

    added fields in clienttenant view model

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/ClientTenantViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/ClientTenantViewModel.cs
@@ -8,11 +8,15 @@ namespace Strive.BusinessEntities.ViewModel
 {
     public class ClientTenantViewModel
     {
+        public int ClientId { get; set; }
         public string ClientName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
         public string MobileNumber { get; set; }
        
         public string ClientEmail { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public DateTime SubscriptionDate { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
