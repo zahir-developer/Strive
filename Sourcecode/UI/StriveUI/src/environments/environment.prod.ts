7d197e245925179aba0762923f1ea6e8ffe9f53e
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2021-06-02 14:38:49 +0530

    Env. prod ts changes.

--- Sourcecode/UI/StriveUI/src/environments/environment.prod.ts
+++ Sourcecode/UI/StriveUI/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api: { // API Endpoints
     striveUrl:  'http://40.114.79.101:5004/',
-    striveCdn: 'http://40.114.79.101:5005/'
+    striveCdn: 'http://40.114.79.101:5005/',
     signalR:  'https://95753a6a1022.ngrok.io/',
     //  striveUrl: 'http://14.141.185.75:5006/'
     // striveUrl: 'http://' + location.hostname + ':5001',
