7f1e9a9807194114d3b97319f47efcb7cf758af1
Vetriselvi Jesudas <naveenkumar.dhasarathan@telliant.net>
2021-06-03 02:07:19 +0530

    EOD Report Employee List showing deleted recors Employee List Wash hours and detail hours data was wrong

--- Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetTimeClockEmployeeHourDetail.sql
+++ Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetTimeClockEmployeeHourDetail.sql
@@ -5,6 +5,8 @@
 -- Description:	Returns the time clock details of the Employees based on location and date. Sample EXEC Strivecarsalon.uspGetTimeClockEmployeeHourDetail 2034, '2020-11-17'
 /*
 2021-06-03 -Vetriselvi -Included Day and Location name
+2021-06-03 -Vetriselvi Employee List showing deleted records
+Employee List Wash hours and detail hours data was wrong
 */
 -- =============================================
 CREATE PROCEDURE [StriveCarSalon].[uspGetTimeClockEmployeeHourDetail]
@@ -19,24 +21,25 @@ select
 	e.employeeId,
 	E.FirstName,
 	E.LastName,
-	TC.EventDate,
-	DateDiff(MINUTE,InTime,ISNULL(OutTime,InTime)) LoginTime,
+	TC.EventDate,InTime,OutTime,
+	Replace(CONVERT(VARCHAR(5),DATEADD(minute, DATEDIFF(MI, ISNULL(InTime,OutTime), ISNULL(OutTime,Intime)), 0), 114),':','.')  as LoginTime, 
 	rm.RoleName INTO #Hours_Data
 from tblTimeClock TC 
 inner join tblEmployee E on TC.EmployeeId = E.EmployeeId
 inner join tblRoleMaster rm on rm.RoleMasterId = tc.RoleId
-where LocationId = @locationId and EventDate = @date and tc.InTime is Not NULL
+where LocationId = @LocationId and EventDate = @Date and tc.InTime is Not NULL
+AND ISNULL(TC.IsDeleted,0) = 0 AND tc.IsActive = 1
 
 ;WITH FinalResult AS (	
 SELECT 
 EmployeeId, FirstName, LastName,
-CASE WHEN RoleName='Washer' THEN ISNULL(cast(LoginTime as float),0) ELSE 0 END AS TotalWashHours,
-CASE WHEN RoleName='Detailer' THEN ISNULL(cast(LoginTime as float),0) ELSE 0 END AS TotalDetailHours,
+CASE WHEN RoleName='Washer' THEN ISNULL(cast(LoginTime as DECIMAL(18,2)),0) ELSE 0 END AS TotalWashHours,
+CASE WHEN RoleName='Detailer' THEN ISNULL(cast(LoginTime as DECIMAL(18,2)),0) ELSE 0 END AS TotalDetailHours,
 CASE WHEN RoleName != 'Washer' AND RoleName != 'Detailer' THEN ISNULL(cast(LoginTime as float),0) ELSE 0 END AS OtherHours
 FROM #Hours_Data
 )
 
-Select EmployeeId, FirstName, LastName,Sum(TotalWashHours) WashHours ,SUM(TotalDetailHours) DetailHours, Round(SUM(OtherHours)/60,2) OtherHours from FinalResult
+Select EmployeeId, FirstName, LastName,REPLACE(Sum(TotalWashHours),'.',':') WashHours ,REPLACE(SUM(TotalDetailHours),'.',':')  DetailHours, Round(SUM(OtherHours)/60,2) OtherHours from FinalResult
 GROUP By EmployeeId, FirstName, LastName
 
 SELECT TimeClockId, EmployeeId, RoleId, rm.RoleName, InTime, OutTime, CONVERT(VARCHAR(5), InTime, 108) as TimeIn, CONVERT(VARCHAR(5), OutTime, 108) as TimeOut,tc.Status,tc.EventDate
