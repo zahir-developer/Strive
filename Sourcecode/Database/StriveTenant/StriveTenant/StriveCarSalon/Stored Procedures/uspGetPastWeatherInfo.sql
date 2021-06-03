9ae0c4254f2944fdf7d858f7a2a38b36b7970f70
Vetriselvi Jesudas <naveenkumar.dhasarathan@telliant.net>
2021-06-03 20:58:59 +0530

    Cash Register Setup - Last previous status not fetching

--- Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetPastWeatherInfo.sql
+++ Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetPastWeatherInfo.sql
@@ -2,6 +2,9 @@
 -- Author:		Shalini
 -- Create date: To get past week and month weather prediction
  --[StriveCarSalon].[uspGetPastWeatherInfo] 2034,'2020-12-08', '2020-11-03', '2020-12-26','2020-09-03'
+ /*
+ 06-06-2021 - Vetriselvi - Wash count is not calculated correctly
+ */
 -- =============================================
 CREATE PROCEDURE [StriveCarSalon].[uspGetPastWeatherInfo]
 	(
@@ -20,13 +23,14 @@ BEGIN
 
 DROP TABLE IF EXISTS #WashHours
 
-Select count(1) as WashCount,LocationId into #WashHours from tblJob j 
+Select count(1) as WashCount,LocationId,j.JobDate into #WashHours from tblJob j 
 INNER JOIN GetTable('JobType') JT on JT.valueid = j.JobType
 INNER join GetTable('JobStatus') GT on GT.valueId = j.JobStatus and GT.valuedesc = 'Completed'
-WHERE j.JobDate = @date
-GROUP BY LocationId
+WHERE j.JobDate IN (@date,@lastweek,@lastMonth,@lastThirdMonth) 
+GROUP BY LocationId,j.JobDate
 
 SELECT 
+WP.WeatherId,
 WP.Weather,
 WP.RainProbability,
 WP.PredictedBusiness,
@@ -35,7 +39,7 @@ WP.LocationId ,
 WP.CreatedDate,
 w.WashCount
 FROM [tblWeatherPrediction] WP
-LEFT join #WashHours w on WP.LocationId=w.LocationId
+LEFT join #WashHours w on WP.LocationId=w.LocationId AND CONVERT(VARCHAR(10), wp.CreatedDate, 120) = w.JobDate
 WHERE
 WP.LocationId =@LocationId AND CONVERT(VARCHAR(10), wp.CreatedDate, 120) in (@date,@lastweek,@lastMonth,@lastThirdMonth) 
 
