239fdaa79d27aa4511b15d4a894c1f3df02b3ff0
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2021-06-10 00:32:19 +0530

    API CORS Changes.

--- Sourcecode/API/Admin.API/Startup.cs
+++ Sourcecode/API/Admin.API/Startup.cs
@@ -248,8 +248,16 @@ namespace Admin.API
             app.UseExceptionHandler("/error");
             app.UseAuthentication();
             app.UseStatusCodePages();
-            app.UseCors(builder => builder.WithOrigins("http://14.141.185.75:5000","http://14.141.185.75:5003","http://localhost:4200", "http://localhost:4300", "http://40.114.79.101:5003", "http://40.114.79.101:5000").AllowAnyMethod().AllowCredentials().AllowAnyHeader());
-            //app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://14.141.185.75:5000",
+                "http://14.141.185.75:5003",
+                "http://localhost:4200",
+                "http://localhost:4300",
+                "http://40.114.79.101:5003",
+                "http://40.114.79.101:5000",
+                "http://40.114.79.101:5005",
+                 "http://40.114.79.101:5007",
+                "http://strive.localqa.com/").AllowAnyMethod().AllowCredentials().AllowAnyHeader());
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             // global cors policy
             //app.UseCors(x => x
