678474ba54cc3d507f801e674502776174dcc38a
Shalini Prabhakaran <shalini.prabhakaran@telliant.net>
2021-06-09 16:53:48 +0530

    GetMembershipDiscountStatus

---
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/MembershipDiscountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strive.BusinessEntities.ViewModel
+{
+    public class MembershipDiscountViewModel
+    {
+        
+        public bool? IsDiscount { get; set; }
+
+    }
+}
