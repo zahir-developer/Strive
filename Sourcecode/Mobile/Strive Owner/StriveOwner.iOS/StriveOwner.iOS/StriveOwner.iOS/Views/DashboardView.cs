a896536268e6ad71c22a4aa8cd6ed0b18a930513
Bhavani <bhavani.annadurai@telliant.net>
2021-07-07 23:19:09 +0530

    Login module completed in Owner

---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.iOS/StriveOwner.iOS/StriveOwner.iOS/Views/DashboardView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Views;
+using Strive.Core.ViewModels.Owner;
+using UIKit;
+
+namespace StriveOwner.iOS.Views
+{
+    public partial class DashboardView : MvxViewController<DashboardViewModel>
+    {
+        public DashboardView() : base("DashboardView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
--- Sourcecode/Mobile/Strive Owner/StriveOwner.iOS/StriveOwner.iOS/StriveOwner.iOS/Views/Login/LoginView.cs
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.iOS/StriveOwner.iOS/StriveOwner.iOS/Views/Login/LoginView.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using Foundation;
 using MvvmCross.Platforms.Ios.Views;
 using Strive.Core.ViewModels.Owner;
 using StriveOwner.iOS.UIUtils;
+using MvvmCross.Binding.BindingContext;
 using UIKit;
 
 namespace StriveOwner.iOS.Views.Login
 {
     public partial class LoginView : MvxViewController<LoginViewModel>
     {
+        NSUserDefaults Persistance;
+        string UsernameKey = "username";
+        string PasswordKey = "password";
+
         public LoginView() : base("LoginView", null)
         {
         }
@@ -16,6 +22,11 @@ namespace StriveOwner.iOS.Views.Login
         {
             base.ViewDidLoad();
             DoInitialSetup();
+            var set = this.CreateBindingSet<LoginView, LoginViewModel>();
+            set.Bind(EmailTextfield).To(vm => vm.loginEmailPhone);
+            set.Bind(PasswordTextfield).To(vm => vm.loginPassword);
+            set.Bind(LoginBtn).To(vm => vm.Commands["DoLogin"]);
+            set.Apply();
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
@@ -27,12 +38,67 @@ namespace StriveOwner.iOS.Views.Login
 
         void DoInitialSetup()
         {
-            NavigationController.NavigationBar.TitleTextAttributes = new UIStringAttributes()
+            NavigationController.NavigationBarHidden = true;
+            var Tap = new UITapGestureRecognizer(() => View.EndEditing(true));
+            Tap.CancelsTouchesInView = false;
+            View.AddGestureRecognizer(Tap);
+            Persistance = NSUserDefaults.StandardUserDefaults;
+            SetCredentials();
+            LoginBtn.Layer.CornerRadius = 5;
+        }
+
+        partial void CheckBoxClicked(UIButton sender)
+        {
+            ViewModel.RememberMeButtonCommand();
+            SetRememberMe();
+        }
+
+        void SetCredentials()
+        {
+            var username = Persistance.StringForKey(UsernameKey);
+            var password = Persistance.StringForKey(PasswordKey);
+            if (username != null && password != null)
             {
-                Font = DesignUtils.OpenSansBoldFifteen(),
-                ForegroundColor = UIColor.Clear.FromHex(0x24489A),
-            };
-            NavigationItem.Title = "Login";
+                ViewModel.loginEmailPhone = username;
+                ViewModel.loginPassword = password;
+                ViewModel.rememberMe = true;
+                SetRememberMe();
+            }
+        }
+
+        void SetRememberMe()
+        {
+            if (ViewModel.rememberMe)
+            {
+                CheckBox.SetImage(UIImage.FromBundle("icon-checked"), UIControlState.Normal);
+                StoreCredentials();
+                return;
+            }
+            ClearCredentials();
+            CheckBox.SetImage(UIImage.FromBundle("icon-unchecked"), UIControlState.Normal);
+        }
+
+        void ClearCredentials()
+        {
+            var dictionary = Persistance.ToDictionary();
+            foreach (var dict in dictionary)
+            {
+                Persistance.RemoveObject(dict.Key.ToString());
+            }
+        }
+
+        void StoreCredentials()
+        {
+            Persistance.SetString(ViewModel.loginEmailPhone, UsernameKey);
+            Persistance.SetString(ViewModel.loginPassword, PasswordKey);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            if (ViewModel.rememberMe)
+            {
+                StoreCredentials();
+            }
         }
     }
 }
