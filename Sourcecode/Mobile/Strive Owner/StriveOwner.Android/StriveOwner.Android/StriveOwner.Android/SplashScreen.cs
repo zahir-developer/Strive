045b8afe9722fd856a132d542cf298e950a5bb63
Ganesh <ganesh.kubendrababu@telliant.net>
2021-07-08 03:16:34 +0800

    Bug fixes 1

---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace StriveOwner.Android
+{
+    [MvxActivityPresentation]
+    [Activity(
+       MainLauncher = true,
+       NoHistory = true,
+       Icon = "@@drawable/strive_logo_page",
+       Theme = "@style/SplashTheme",
+       ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen()
+        {
+        }
+    }
+}
