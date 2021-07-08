705cbe6a3a4070b2e084449ba67bf6d572985f67
Bhavani <bhavani.annadurai@telliant.net>
2021-07-08 22:03:27 +0530

    Dashboard setup in owner

---
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.iOS/StriveOwner.iOS/StriveOwner.iOS/Views/CheckOut/CheckOutView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Views;
+using Strive.Core.ViewModels.Owner;
+using UIKit;
+
+namespace StriveOwner.iOS.Views.CheckOut
+{
+    public partial class CheckOutView : MvxViewController<CheckOutViewModel>
+    {
+        public CheckOutView() : base("CheckOutView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ Sourcecode/Mobile/Strive.Core/Strive.Core/ViewModels/Owner/CheckOutViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Strive.Core.ViewModels.Owner
+{
+    public class CheckOutViewModel : BaseViewModel
+    {
+        public CheckOutViewModel()
+        {
+        }
+    }
+}
