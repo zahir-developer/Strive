77714a94923e2ff11712fcf3f4d3e9adf9877e80
Karthik <karthik.v@telliant.net>
2021-07-24 13:23:49 +0530

    Add CustomTextField that restrict paste operation

---
+++ Sourcecode/Mobile/Strive Greeter/Greeter/CustomView/CustomTextField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace Greeter.CustomView
+{
+    public class CustomTextField : UITextField
+    {
+        public CustomTextField(): base() { }
+
+        public CustomTextField(CGRect rect) : base(rect) { }
+
+        public CustomTextField(IntPtr ptr) : base(ptr) { }
+
+        public CustomTextField(NSObjectFlag flag) : base(flag) { }
+
+        public CustomTextField(NSCoder coder) : base(coder) { }
+
+        public override bool CanPerform(Selector action, NSObject withSender)
+        {
+            if (action == new Selector("paste:")) return false;
+
+            return base.CanPerform(action, withSender);
+        }
+    }
+}
