ab55dc7ef1f73639ba1251e99ed31a0dc6903ce0
Zahir Hussain Mohamed Nazar <zahir.mohamed@telliant.net>
2021-07-29 14:30:11 +0530

    Exprie token minutes added in login api.

--- Sourcecode/API/Strive.BusinessEntities/DTO/Employee/EmployeeLoginDto.cs
+++ Sourcecode/API/Strive.BusinessEntities/DTO/Employee/EmployeeLoginDto.cs
@@ -14,8 +14,8 @@ namespace Strive.BusinessEntities.DTO.Employee
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public int AuthId { get; set; }
-        public int ClientId { get; set; }
-        public int ClientAuthId { get; set; }
+        public int? ClientId { get; set; }
+        public int? ClientAuthId { get; set; }
         public bool IsActive { get; set; }
     }
 }
