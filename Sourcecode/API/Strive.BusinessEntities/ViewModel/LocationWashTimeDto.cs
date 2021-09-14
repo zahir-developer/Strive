69e9602af9a052574494622e1786dd9f09c52e9a
zmohamed <zahir.mohamed@telliant.net>
2021-09-14 20:17:53 +0530

    GetAllLocation washtime - additional fields added. Client email added - GetVehicle&Clientdetail using barcode.

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/LocationWashTimeDto.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/LocationWashTimeDto.cs
@@ -10,12 +10,18 @@ namespace Strive.BusinessEntities.ViewModel
     {
         public int LocationId { get; set; }
         public string LocationName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public int WashtimeMinutes { get; set; }
         public string StoreStatus { get; set; }
         public DateTimeOffset StoreTimeIn { get; set; }
         public DateTimeOffset StoreTimeOut { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
+
+        
 
     }
 }
