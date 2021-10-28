0a2c768d83693dd7ac125f5a5e6808d2cfcf5a83
zmohamed <zahir.mohamed@telliant.net>
2021-10-29 00:29:57 +0530

    Customer summary report changes.

--- Sourcecode/API/Strive.BusinessEntities/ViewModel/CustomerSummaryViewModel.cs
+++ Sourcecode/API/Strive.BusinessEntities/ViewModel/CustomerSummaryViewModel.cs
@@ -11,10 +11,14 @@ namespace Strive.BusinessEntities.ViewModel
         public int Month { get; set; }
         public int? NumberOfMembershipAccounts { get; set; }
         public int? CustomerCount { get; set; }
+        public int? VehicleCount { get; set; }
         public int? WashesCompletedCount { get; set; }
         public decimal? AverageNumberOfWashesPerCustomer { get; set; }
+        public decimal? AverageNumberOfWashesPerVehicle { get; set; }
         public decimal? TotalNumberOfWashesPerCustomer { get; set; }
+        public decimal? TotalNumberOfWashesPerVehicle { get; set; }
         public decimal? PercentageOfCustomersThatTurnedUp { get; set; }
+        public decimal? PercentageOfVehicleThatTurnedUp { get; set; }
 
 
     }
