988a6a5f6d267a548a1cff0a03678265268a97bf
Sougatha <sougatha.b@telliant.net>
2021-11-24 20:26:50 +0530

    Added additional services in membership creation

--- Sourcecode/Mobile/Strive.Core/Strive.Core/ViewModels/Customer/PaymentViewModel.cs
+++ Sourcecode/Mobile/Strive.Core/Strive.Core/ViewModels/Customer/PaymentViewModel.cs
@@ -1,14 +1,21 @@
-﻿using Strive.Core.Models.Customer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Strive.Core.Models.Customer;
+using Strive.Core.Models.TimInventory;
 
 namespace Strive.Core.ViewModels.Customer
 {
     public class PaymentViewModel : BaseViewModel
     {
+        public List<ClientVehicleMembershipService> selectedmembershipServices = new List<ClientVehicleMembershipService>();
+       
+        
         public PaymentViewModel()
         {
 
         }
-          
+        
 
         public async void MembershipAgree()
         {
@@ -20,7 +27,7 @@ namespace Strive.Core.ViewModels.Customer
                     var isDeleted = await AdminService.DeleteVehicleMembership(CustomerVehiclesInformation.completeVehicleDetails.VehicleMembershipDetails.ClientVehicleMembership.ClientMembershipId);
                 }
             }
-
+            PrepareAdditionalServices();
             var data = await AdminService.SaveVehicleMembership(MembershipDetails.customerVehicleDetails);
             if (data.Status == true)
             {
@@ -34,6 +41,24 @@ namespace Strive.Core.ViewModels.Customer
                 _userDialog.Alert("Error membership not created");
             }
             _userDialog.HideLoading();
+        }
+
+        private void PrepareAdditionalServices()
+        {
+            var servicesDetails = MembershipDetails.completeList.ServicesWithPrice.Where(x => MembershipDetails.selectedAdditionalServices.Contains(x.ServiceId)).ToList();
+            foreach(var service in servicesDetails)
+            {
+                ClientVehicleMembershipService clientVehicleMembershipService = new ClientVehicleMembershipService();
+                clientVehicleMembershipService.serviceId = service.ServiceId;
+                clientVehicleMembershipService.serviceTypeId = service.ServiceTypeId;
+                clientVehicleMembershipService.createdDate = DateTime.Now.ToString();
+                clientVehicleMembershipService.updatedDate = DateTime.Now.ToString();
+                clientVehicleMembershipService.clientMembershipId = service.MembershipId;
+                clientVehicleMembershipService.clientVehicleMembershipServiceId = service.MembershipServiceId;
+                selectedmembershipServices.Add(clientVehicleMembershipService);
+            }
+
+            MembershipDetails.customerVehicleDetails.clientVehicleMembershipModel.clientVehicleMembershipService = selectedmembershipServices;
         }
         public enum PaymentStatus
         {
