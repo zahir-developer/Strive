b5289ce1bd69a878524c0d95f68270f52215fd45
psundarraj <psundarraj@TELLIANT81>
2021-12-20 03:29:17 -0500

    Email Batch Checkin 3

--- Sourcecode/API/StriveEmailScheduler/AppStartup.cs
+++ Sourcecode/API/StriveEmailScheduler/AppStartup.cs
@@ -1,5 +1,8 @@
-﻿using Strive.BusinessLogic;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Strive.BusinessLogic;
 using Strive.BusinessLogic.Client;
+using Strive.Common;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +12,9 @@ namespace StriveEmailScheduler
 
     class AppStartup
     {
+        private readonly IConfiguration _config;
+        private readonly IDistributedCache _cache;
+        readonly ITenantHelper _tenant;
         private readonly IClientBpl _clientBpl;
         private readonly IEmployeeBpl _employeeBpl;
         public AppStartup(IClientBpl clientBpl, IEmployeeBpl employeeBpl)
@@ -22,12 +28,13 @@ namespace StriveEmailScheduler
         {
             try
             {
+               // var abc = new ClientBpl(_cache, _tenant);
                 //if (args != null && args.Length > 0)
                 //{
                 //    if (!string.IsNullOrWhiteSpace(args[0]))
                 //    {
-                       _clientBpl.SendClientEmail();
-                            _employeeBpl.SendEmployeeEmail();
+                _clientBpl.SendClientEmail();
+                          //  _employeeBpl.SendEmployeeEmail();
                         
                 //    }
                 //}
--- Sourcecode/API/StriveEmailScheduler/Program.cs
+++ Sourcecode/API/StriveEmailScheduler/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Strive.BusinessLogic;
 using Strive.BusinessLogic.Client;
+using Strive.Common;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace StriveEmailScheduler
 {
@@ -11,17 +11,25 @@ namespace StriveEmailScheduler
     {
         public static void Main(string[] args)
         {
-            var builder = new HostBuilder()
+             //  IDistributedCache _cache;
+              var builder = new HostBuilder()
              .ConfigureServices((hostContext, services) =>
              {
-                 services.AddTransient<IClientBpl, ClientBpl>();
-                 services.AddTransient<IEmployeeBpl, EmployeeBpl>();
+                 services.AddScoped<ITenantHelper>(_ => new TenantHelper(null));
                  ServiceProvider serviceProvider = services.BuildServiceProvider();
-                 var clientBpl = serviceProvider.GetService<IClientBpl>();
+                 var tenant = serviceProvider.GetService<ITenantHelper>();
+                 // services.AddSingleton(typeof(IDistributedCache), typeof(DistributedCache));
+                
+                 services.AddTransient<IClientBpl>(_ => new ClientBpl(null, tenant));
+                 //services.AddTransient<IClientBpl, ClientBpl>(_ => _cache);
+                 services.AddTransient<IEmployeeBpl>(_ => new EmployeeBpl(null, tenant));
+                                 var clientBpl = serviceProvider.GetService<IClientBpl>();
                  var employeeBpl = serviceProvider.GetService<IEmployeeBpl>();
                  new AppStartup(clientBpl, employeeBpl).Execute(args);
              });
             //  CreateHostBuilder(args).Build().Run();
+            //  await builder.RunConsoleAsync();
+            builder.Build().Run();
 
         }
 
