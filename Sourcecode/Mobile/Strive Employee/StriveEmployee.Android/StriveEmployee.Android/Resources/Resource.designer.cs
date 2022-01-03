e6226703d26a5782073791acbd673dc87b1f0bf9
sangeethadevicd <sangeetha.cd@telliant.net>
2022-01-03 20:37:03 +0300

    icon fix in employee app

--- Sourcecode/Mobile/Strive Employee/StriveEmployee.Android/StriveEmployee.Android/Resources/Resource.designer.cs
+++ Sourcecode/Mobile/Strive Employee/StriveEmployee.Android/StriveEmployee.Android/Resources/Resource.designer.cs
@@ -11929,121 +11929,124 @@ namespace StriveEmployee.Android
 			public const int mobile_portrait_bg = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int mtrl_snackbar_background = 2131165310;
+			public const int money = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int mtrl_tabs_default_indicator = 2131165311;
+			public const int mtrl_snackbar_background = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int navigation_empty_icon = 2131165312;
+			public const int mtrl_tabs_default_indicator = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int notification_action_background = 2131165313;
+			public const int navigation_empty_icon = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int notification_bg = 2131165314;
+			public const int notification_action_background = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int notification_bg_low = 2131165315;
+			public const int notification_bg = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int notification_bg_low_normal = 2131165316;
+			public const int notification_bg_low = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int notification_bg_low_pressed = 2131165317;
+			public const int notification_bg_low_normal = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int notification_bg_normal = 2131165318;
+			public const int notification_bg_low_pressed = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int notification_bg_normal_pressed = 2131165319;
+			public const int notification_bg_normal = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int notification_icon_background = 2131165320;
+			public const int notification_bg_normal_pressed = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int notification_template_icon_bg = 2131165321;
+			public const int notification_icon_background = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int notification_template_icon_low_bg = 2131165322;
+			public const int notification_template_icon_bg = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int notification_tile_bg = 2131165323;
+			public const int notification_template_icon_low_bg = 2131165323;
 			
 			// aapt resource value: 0x7F07008C
-			public const int notify_panel_notification_icon_bg = 2131165324;
+			public const int notification_tile_bg = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int paidCheck = 2131165325;
+			public const int notify_panel_notification_icon_bg = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int paperclip = 2131165326;
+			public const int paidCheck = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int payroll = 2131165327;
+			public const int paperclip = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int Primary_Button = 2131165328;
+			public const int payroll = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int profile = 2131165329;
-			
-			// aapt resource value: 0x7F070093
-			public const int profileAvatar_Button = 2131165331;
+			public const int Primary_Button = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int profile_white = 2131165330;
+			public const int profile = 2131165330;
 			
 			// aapt resource value: 0x7F070094
-			public const int racing_flag = 2131165332;
+			public const int profileAvatar_Button = 2131165332;
+			
+			// aapt resource value: 0x7F070093
+			public const int profile_white = 2131165331;
 			
 			// aapt resource value: 0x7F070095
-			public const int Recipient_ChatBubble = 2131165333;
+			public const int racing_flag = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int remove = 2131165334;
+			public const int Recipient_ChatBubble = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int roundedbg = 2131165335;
+			public const int remove = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int roundedbgdark = 2131165336;
-			
-			// aapt resource value: 0x7F07009A
-			public const int SecondaryLinearLayout = 2131165338;
+			public const int roundedbg = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int Secondary_Button = 2131165337;
+			public const int roundedbgdark = 2131165337;
 			
 			// aapt resource value: 0x7F07009B
-			public const int sendcircle = 2131165339;
+			public const int SecondaryLinearLayout = 2131165339;
+			
+			// aapt resource value: 0x7F07009A
+			public const int Secondary_Button = 2131165338;
 			
 			// aapt resource value: 0x7F07009C
-			public const int Sender_ChatBubble = 2131165340;
+			public const int sendcircle = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int Splash_Screen = 2131165341;
+			public const int Sender_ChatBubble = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int strive_logo_page = 2131165342;
+			public const int Splash_Screen = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int strive_logo_splash = 2131165343;
+			public const int strive_logo_page = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int ticket = 2131165344;
+			public const int strive_logo_splash = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int tooltip_frame_dark = 2131165345;
+			public const int ticket = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int tooltip_frame_light = 2131165346;
+			public const int tooltip_frame_dark = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int Uncheck = 2131165347;
+			public const int tooltip_frame_light = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int view = 2131165348;
+			public const int Uncheck = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int view = 2131165349;
 			
 			static Drawable()
 			{
