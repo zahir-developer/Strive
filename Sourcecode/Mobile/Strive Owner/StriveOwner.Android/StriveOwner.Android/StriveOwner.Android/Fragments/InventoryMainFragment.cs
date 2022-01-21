eb7c63b793c685a84d5c901ea52f09299876b6ba
sangeethadevicd <sangeetha.cd@telliant.net>
2022-01-22 02:29:46 +0300

    owner app fix

--- Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/Fragments/InventoryMainFragment.cs
+++ Sourcecode/Mobile/Strive Owner/StriveOwner.Android/StriveOwner.Android/StriveOwner.Android/Fragments/InventoryMainFragment.cs
@@ -1,40 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.Graphics;
-using Android.Graphics.Drawables;
-using Android.OS;
-using Android.Runtime;
-using Android.Support.V4.Content;
-=======
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
->>>>>>> Stashed changes
 using Android.Support.V7.Widget;
-using Android.Support.V7.Widget.Helper;
-using Android.Util;
 using Android.Views;
-using Android.Widget;
-using MvvmCross.Binding.BindingContext;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using Strive.Core.Models.TimInventory;
 using Strive.Core.ViewModels.Owner;
 using StriveOwner.Android.Adapter;
 using StriveOwner.Android.Helper;
-<<<<<<< Updated upstream
-using StriveOwner.Android.Resources.Fragments;
-=======
-
->>>>>>> Stashed changes
 using SearchView = Android.Support.V7.Widget.SearchView;
 
 namespace StriveOwner.Android.Resources.Fragments
@@ -44,16 +22,6 @@ namespace StriveOwner.Android.Resources.Fragments
         private InventoryMainAdapter inventoryMainAdapter;
         private RecyclerView inventoryMain_RecyclerView;
         private SearchView inventoryMain_SearchView;
-<<<<<<< Updated upstream
-        private Context context;
-        private AlertDialog.Builder Builder;
-        private EventHandler<DialogClickEventArgs> okHandler;
-        private EventHandler<DialogClickEventArgs> removePhotoHandler;
-
-        public InventoryMainFragment(Context context)
-        {
-            this.context = context;
-=======
         private Context Context;
         private AlertDialog.Builder Builder;
         private EventHandler<DialogClickEventArgs> okHandler;
@@ -63,10 +31,8 @@ namespace StriveOwner.Android.Resources.Fragments
         public InventoryMainFragment(Context context)
         {
             this.Context = context;
->>>>>>> Stashed changes
         }
 
-        // private SwipeController swipeController;
 
 
         public override void OnCreate(Bundle savedInstanceState)
@@ -90,28 +56,10 @@ namespace StriveOwner.Android.Resources.Fragments
         }
 
         private async void InventoryMain_SearchView_QueryTextChange(object sender, SearchView.QueryTextChangeEventArgs e)
-<<<<<<< Updated upstream
         {
             await ViewModel.InventorySearchCommand(e.NewText);
             InventoryAdapterData();
         }
-
-        private async void GetProducts()
-=======
->>>>>>> Stashed changes
-        {
-            await ViewModel.InventorySearchCommand(e.NewText);
-            InventoryAdapterData();
-        }
-<<<<<<< Updated upstream
-        public void OnRightClicked(int position)
-        {
-            this.ViewModel.FilteredList.RemoveAt(position);
-            inventoryMainAdapter.NotifyItemRemoved(position);
-            inventoryMainAdapter.NotifyItemRangeChanged(position, inventoryMainAdapter.ItemCount);
-        }
-        private void InventoryAdapterData()
-=======
 
         private async void GetProducts()
         {
@@ -121,7 +69,7 @@ namespace StriveOwner.Android.Resources.Fragments
                 ViewModel.ClearCommand();
                 await ViewModel.InventorySearchCommand("");
             }
-             InventoryAdapterData();
+            InventoryAdapterData();
         }
 
         private async Task<bool> GetVendors()
@@ -129,36 +77,21 @@ namespace StriveOwner.Android.Resources.Fragments
             await ViewModel.GetVendorsCommand();
             return true;
         }
-        private  void InventoryAdapterData()
->>>>>>> Stashed changes
+
+        private void InventoryAdapterData()
         {
-            inventoryMainAdapter = new InventoryMainAdapter(Context, ViewModel.FilteredList , ViewModel);
+            inventoryMainAdapter = new InventoryMainAdapter(Context, ViewModel.FilteredList, ViewModel);
             var layoutManager = new LinearLayoutManager(Context);
             inventoryMain_RecyclerView.SetLayoutManager(layoutManager);
             inventoryMain_RecyclerView.SetAdapter(inventoryMainAdapter);
-<<<<<<< Updated upstream
-            MySwipeHelper mySwipe = new MyImplementSwipeHelper(Context, inventoryMain_RecyclerView, 200, ViewModel.FilteredList);           
-        }
-        public void DeleteItem(InventoryDataModel selectedItem, RecyclerView inventoryMain_RecyclerView)
-        {
-
-            Builder = new AlertDialog.Builder(Context);
-            Builder.SetMessage("Are you sure want to change the status to hold?");
-            Builder.SetTitle("Hold");
-            okHandler = new EventHandler<DialogClickEventArgs>((object s, DialogClickEventArgs de) =>
+            if (ViewModel.FilteredList != null && ViewModel.FilteredList.Count > 0 && !isSwipeCalled)
             {
-                DeleteFromList(selectedItem, inventoryMain_RecyclerView);
-=======
-            if(ViewModel.FilteredList!=null && ViewModel.FilteredList.Count > 0 && !isSwipeCalled)
-            {
-                MySwipeHelper mySwipe = new MyImplementSwipeHelper(Context, inventoryMain_RecyclerView, 200 ,this.ViewModel);
+                MySwipeHelper mySwipe = new MyImplementSwipeHelper(Context, inventoryMain_RecyclerView, 200, this.ViewModel);
                 isSwipeCalled = true;
             }
-
         }
 
-
-        private  void InventoryAdapterDataAsync(RecyclerView recyclerView, InventoryViewModel viewModel)
+        private void InventoryAdapterDataAsync(RecyclerView recyclerView, InventoryViewModel viewModel)
         {
             inventoryMain_RecyclerView = recyclerView;
             inventoryMainAdapter = new InventoryMainAdapter(Context, viewModel.FilteredList, viewModel);
@@ -166,7 +99,7 @@ namespace StriveOwner.Android.Resources.Fragments
             inventoryMain_RecyclerView.SetLayoutManager(layoutManager);
             inventoryMain_RecyclerView.SetAdapter(inventoryMainAdapter);
         }
-       
+
 
         public void DeleteItem(InventoryDataModel selectedItem, RecyclerView inventoryMain_RecyclerView, InventoryViewModel viewModel)
         {
@@ -176,33 +109,19 @@ namespace StriveOwner.Android.Resources.Fragments
             Builder.SetTitle("Delete");
             okHandler = new EventHandler<DialogClickEventArgs>((object s, DialogClickEventArgs de) =>
             {
-                DeleteFromList(selectedItem, inventoryMain_RecyclerView,viewModel);
->>>>>>> Stashed changes
+                DeleteFromList(selectedItem, inventoryMain_RecyclerView, viewModel);
             });
             removePhotoHandler = new EventHandler<DialogClickEventArgs>((object s, DialogClickEventArgs de) =>
             {
 
             });
-<<<<<<< Updated upstream
             Builder.SetPositiveButton("Ok", okHandler);
             Builder.SetNegativeButton("Cancel", removePhotoHandler);
             Builder.Create();
             Builder.Show();
         }
-        public async void DeleteFromList(InventoryDataModel selectedItem, RecyclerView recyclerView)
-        {
-            RecyclerView recyclerView1;
-            ViewModel = new InventoryViewModel();
-            int index = ViewModel.FilteredList.IndexOf(selectedItem);
-            await ViewModel.DeleteProductCommand(index);
-            inventoryMainAdapter.NotifyDataSetChanged();            
-=======
-            Builder.SetPositiveButton("Yes", okHandler);
-            Builder.SetNegativeButton("No", removePhotoHandler);
-            Builder.Create();
-            Builder.Show();
-        }
-        public async void DeleteFromList(InventoryDataModel selectedItem, RecyclerView recyclerView , InventoryViewModel viewModel)
+
+        public async void DeleteFromList(InventoryDataModel selectedItem, RecyclerView recyclerView, InventoryViewModel viewModel)
         {
             RecyclerView recyclerView1;
             int index = viewModel.FilteredList.IndexOf(selectedItem);
@@ -210,11 +129,13 @@ namespace StriveOwner.Android.Resources.Fragments
             if (response)
             {
                 await viewModel.InventorySearchCommand(" ");
-                InventoryAdapterDataAsync(recyclerView,viewModel);
+                InventoryAdapterDataAsync(recyclerView, viewModel);
             }
-           
+
         }
-        private class MyImplementSwipeHelper : MySwipeHelper
+    }
+
+        public class MyImplementSwipeHelper : MySwipeHelper
         {
             Context Context;
             RecyclerView inventoryMain_RecyclerView;
@@ -267,69 +188,7 @@ namespace StriveOwner.Android.Resources.Fragments
                 }
 
             }
->>>>>>> Stashed changes
         }
-        private class MyImplementSwipeHelper : MySwipeHelper
-        {
-            Context Context;
-            RecyclerView inventoryMain_RecyclerView;
-            int buttonWidth;
-            ObservableCollection<InventoryDataModel> filteredlist;
 
-            public MyImplementSwipeHelper(Context context, RecyclerView inventoryMain_RecyclerView, int buttonWidth, ObservableCollection<InventoryDataModel> filteredlist) : base(context, inventoryMain_RecyclerView, 200, filteredlist)
-            {
-                Context = context;
-                this.inventoryMain_RecyclerView = inventoryMain_RecyclerView;
-                this.buttonWidth = buttonWidth;
-                this.filteredlist = filteredlist;
-            }
-
-            public override void InstantiateMyButton(RecyclerView.ViewHolder viewHolder, List<MyButton> buffer)
-            {
-                int itemposition = viewHolder.AdapterPosition;
-                var selectedItem = filteredlist[itemposition];
-                InventoryMainFragment fragment = new InventoryMainFragment(Context);
-                //button-1
-                buffer.Add(new MyButton(Context,
-                    "Delete",
-                    35,
-                    Resource.Drawable.ic_errorstatus,
-                    "#FF0000",
-                    new DeleteButtonClick(this, Context, selectedItem, fragment, inventoryMain_RecyclerView), selectedItem));
-
-            }
-            public override void InstantiateUpdatedMyButton(RecyclerView.ViewHolder viewHolder, List<MyButton> buffer)
-            {
-
-            }
-            public class DeleteButtonClick : DeleteButtonClickListener
-            {
-                private MyImplementSwipeHelper myImplementSwipeHelper;
-
-                RecyclerView inventoryMain_RecyclerView;
-
-                public AlertDialog.Builder Builder;
-                private EventHandler<DialogClickEventArgs> okHandler;
-                private EventHandler<DialogClickEventArgs> removePhotoHandler;
-                Context Context;
-                InventoryDataModel filteredlist;
-                InventoryMainFragment fragment;
-
-                public DeleteButtonClick(MyImplementSwipeHelper myImplementSwipeHelper, Context context, InventoryDataModel filteredlist, InventoryMainFragment fragment, RecyclerView recyclerView)
-                {
-                    this.myImplementSwipeHelper = myImplementSwipeHelper;
-                    this.Context = context;
-                    this.filteredlist = filteredlist;
-                    this.fragment = fragment;
-                    this.inventoryMain_RecyclerView = recyclerView;
-                }
-
-                public void OnClick(int position, InventoryDataModel selectedItem)
-                {
-                    fragment.DeleteItem(selectedItem, inventoryMain_RecyclerView);
-                }
-            }
-        }        
-
-    }
+    
 }
