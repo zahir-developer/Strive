38530addd9a503994814928a9a94dacfcc186d05
Juki Baskar <juki.b@telliant.net>
2022-03-31 17:57:34 +0530

    Added new table tblPrinter and new SP uspGetPrinterByLocation

---
+++ Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Stored Procedures/uspGetPrinterByLocation.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================  
+-- Author:  JUKI B  
+-- Create date: 31-03-2022  
+-- Description: To fetch printer detail by location
+-- =============================================  
+---------------------History--------------------  
+-- =============================================  
+-- 31-03-2022, Juki - Added  
+------------------------------------------------  
+CREATE PROCEDURE uspGetPrinterByLocation 
+(@Location int)
+AS
+BEGIN
+Select PrinterId, PrinterName, IpAddress, LocationId from tblPrinter
+where LocationId = IIF(@Location IS NULL, LocationId, @Location) 
+END
---
+++ Sourcecode/Database/StriveTenant/StriveTenant/StriveCarSalon/Tables/tblPrinter.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblPrinter]
+(
+	[PrinterId] INT NOT NULL PRIMARY KEY, 
+    [PrinterName] VARCHAR(64) NULL, 
+    [IpAddress] VARCHAR(20) NULL,
+	[IsActive] [bit] NULL,
+	[IsDeleted] [bit] NULL,
+	[CreatedBy] [int] NULL,
+	[CreatedDate] [datetimeoffset](7) NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedDate] [datetimeoffset](7) NULL,
+	LocationId int,
+	CONSTRAINT FK_PrinterLocation
+	FOREIGN KEY (LocationId) REFERENCES tblLocation(LocationId)
+)
